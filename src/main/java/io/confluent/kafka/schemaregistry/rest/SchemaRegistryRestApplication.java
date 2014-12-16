edbd6c1d67baee5b39bcbd36e1a3f7e613e34b34
Geoff Anderson <geoff@confluent.io>
2014-12-16 14:41:31 -0800

    Moves hard-coded configs from SchemaRegistryRestApplication.java to SchemaRegistryConfig.java

--- src/main/java/io/confluent/kafka/schemaregistry/rest/SchemaRegistryRestApplication.java
+++ src/main/java/io/confluent/kafka/schemaregistry/rest/SchemaRegistryRestApplication.java
@@ -1,10 +1,8 @@
 package io.confluent.kafka.schemaregistry.rest;
 
-import io.confluent.kafka.schemaregistry.rest.entities.Schema;
 import io.confluent.kafka.schemaregistry.rest.resources.RootResource;
 import io.confluent.kafka.schemaregistry.rest.resources.SchemasResource;
 import io.confluent.kafka.schemaregistry.rest.resources.TopicsResource;
-import io.confluent.kafka.schemaregistry.storage.KafkaStoreConfig;
 import io.confluent.kafka.schemaregistry.storage.SchemaRegistry;
 import io.confluent.kafka.schemaregistry.storage.SchemaRegistryConfig;
 import io.confluent.kafka.schemaregistry.storage.serialization.SchemaSerializer;
@@ -29,12 +27,9 @@ public class SchemaRegistryRestApplication extends Application<SchemaRegistryRes
 
     @Override
     public void setupResources(Configurable<?> config, SchemaRegistryRestConfiguration appConfig) {
-        Properties props = new Properties();
-        props.put(KafkaStoreConfig.KAFKASTORE_CONNECTION_URL_CONFIG, "localhost:2181");
-        props.put(KafkaStoreConfig.KAFKASTORE_TOPIC_CONFIG, "schemaregistry");
-        SchemaRegistryConfig schemaRegistryConfig = new SchemaRegistryConfig(props);
-        SchemaRegistry schemaRegistry = new SchemaRegistry(schemaRegistryConfig,
-            new SchemaSerializer());
+        SchemaRegistryConfig schemaRegistryConfig = new SchemaRegistryConfig(new Properties());
+        SchemaRegistry schemaRegistry =
+            new SchemaRegistry(schemaRegistryConfig, new SchemaSerializer());
         config.register(RootResource.class);
         config.register(new TopicsResource(schemaRegistry));
         config.register(SchemasResource.class);
