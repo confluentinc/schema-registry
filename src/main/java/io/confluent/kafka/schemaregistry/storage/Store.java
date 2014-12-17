e298c1322ea7d2d3b6718b27a10a5d447be534b0
Geoff Anderson <geoff@confluent.io>
2014-12-17 11:07:15 -0800

    Tweak Store.java comments.

--- src/main/java/io/confluent/kafka/schemaregistry/storage/Store.java
+++ src/main/java/io/confluent/kafka/schemaregistry/storage/Store.java
@@ -25,8 +25,8 @@ public interface Store<K, V> {
    *
    * @param key1 If key1 is null, start from the first key in sorted order
    * @param key2 If key2 is null, end at the last key
-   * @return Iterator over keys in range (key1, key2]. If both keys are null, return an iterator
-   * over all keys in the database
+   * @return Iterator over keys in range the half-open interval [key1, key2). If both keys are
+   * null, return an iterator over all keys in the database
    */
   public Iterator<V> getAll(K key1, K key2) throws StoreException;
 
