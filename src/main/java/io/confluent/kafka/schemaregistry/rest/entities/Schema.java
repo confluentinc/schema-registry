296cfdad8a9daf711bcbd4d02a00d7c738594ea5
Neha Narkhede <neha.narkhede@gmail.com>
2014-12-18 18:33:04 -0800

    Issue #14: Clean up flags in Schema.java: reviewed by Geoff

--- src/main/java/io/confluent/kafka/schemaregistry/rest/entities/Schema.java
+++ src/main/java/io/confluent/kafka/schemaregistry/rest/entities/Schema.java
@@ -14,22 +14,16 @@ public class Schema {
   private Integer version;
   @NotEmpty
   private String schema;
-  private boolean compatible = true;
   private boolean deprecated = false;
-  private boolean latest = true;
 
   public Schema(@JsonProperty("name") String name,
                 @JsonProperty("version") Integer version,
                 @JsonProperty("schema") String schema,
-                @JsonProperty("compatible") boolean compatible,
-                @JsonProperty("deprecated") boolean deprecated,
-                @JsonProperty("latest") boolean latest) {
+                @JsonProperty("deprecated") boolean deprecated) {
     this.name = name;
     this.version = version;
     this.schema = schema;
-    this.compatible = compatible;
     this.deprecated = deprecated;
-    this.latest = latest;
   }
 
   @JsonProperty("name")
@@ -62,16 +56,6 @@ public class Schema {
     this.version = version;
   }
 
-  @JsonProperty("compatible")
-  public boolean getCompatible() {
-    return this.compatible;
-  }
-
-  @JsonProperty("compatible")
-  public void setCompatible(boolean compatible) {
-    this.compatible = compatible;
-  }
-
   @JsonProperty("deprecated")
   public boolean getDeprecated() {
     return this.deprecated;
@@ -80,16 +64,6 @@ public class Schema {
   @JsonProperty("deprecated")
   public void setDeprecated(boolean deprecated) {
     this.deprecated = deprecated;
-  }
-
-  @JsonProperty("latest")
-  public boolean getLatest() {
-    return this.latest;
-  }
-
-  @JsonProperty("latest")
-  public void setLatest(boolean latest) {
-    this.latest = latest;
   }
 
   @Override
@@ -112,13 +86,7 @@ public class Schema {
     if (this.version != that.getVersion()) {
       return false;
     }
-    if (this.compatible && !that.compatible) {
-      return false;
-    }
     if (this.deprecated && !that.deprecated) {
-      return false;
-    }
-    if (this.latest && !that.latest) {
       return false;
     }
 
@@ -130,9 +98,7 @@ public class Schema {
     int result = name.hashCode();
     result = 31 * result + schema.hashCode();
     result = 31 * result + version;
-    result = 31 * result + new Boolean(compatible).hashCode();
     result = 31 * result + new Boolean(deprecated).hashCode();
-    result = 31 * result + new Boolean(latest).hashCode();
     return result;
   }
 
@@ -142,9 +108,7 @@ public class Schema {
     sb.append("{name=" + this.name + ",");
     sb.append("schema=" + this.schema + ",");
     sb.append("version=" + this.version + ",");
-    sb.append("compatible=" + this.compatible + ",");
     sb.append("deprecated=" + this.deprecated + ",");
-    sb.append("latest=" + this.latest + "}");
     return sb.toString();
   }
 
