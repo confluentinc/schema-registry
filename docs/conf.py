475b89ee7703d7426dbf4444a42af147f385d069
Ismael Juma <ismael@juma.me.uk>
2016-02-09 21:09:26 +0000

    Bump the short version in conf.py to `2.1`

--- docs/conf.py
+++ docs/conf.py
@@ -57,7 +57,7 @@ copyright = u'2015, Confluent, Inc.'
 # built documents.
 #
 # The short X.Y version.
-version = '2.0'
+version = '2.1'
 # The full version, including alpha/beta/rc tags.
 release = '2.1.0-SNAPSHOT'
 
