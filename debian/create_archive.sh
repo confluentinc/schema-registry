533897e2acaa58a82e2ad53461307d7a2124ded5
Kaufman Ng <kaufman@confluent.io>
2016-07-13 13:39:43 -0700

    fixed install path of systemd files

--- debian/create_archive.sh
+++ debian/create_archive.sh
@@ -34,7 +34,7 @@ find . -type f | grep -v README[.]rpm | xargs -I XXX ${INSTALL} -o root -g root 
 popd
 popd
 
-find systemd -type f | xargs -I XXX ${INSTALL} -o root -g root XXX ${DESTDIR}${SYSCONFDIR}/XXX
+find systemd -type f | xargs -I XXX ${INSTALL} -o root -g root XXX ${DESTDIR}${SYSTEMDDIR}/XXX
 
 # kafka-serde-tools
 PREPACKAGED_KAFKA_SERDE_TOOLS="package-kafka-serde-tools/target/kafka-serde-tools-package-${VERSION}-package"
