be78b133beec30bdbe28cc6764e50d7889e0b17f
Mateusz Bukowicz <mateusz.bukowicz@nordea.com>
2017-05-12 12:38:30 +0200

    Add draft implementation of authorization filter

--- core/src/main/java/io/confluent/kafka/schemaregistry/rest/SchemaRegistryRestApplication.java
+++ core/src/main/java/io/confluent/kafka/schemaregistry/rest/SchemaRegistryRestApplication.java
@@ -16,11 +16,22 @@
 
 package io.confluent.kafka.schemaregistry.rest;
 
+import jersey.repackaged.com.google.common.collect.ImmutableMap;
+import kafka.security.auth.Acl;
+import kafka.security.auth.Resource;
+import kafka.security.auth.SimpleAclAuthorizer;
+import kafka.security.auth.Write$;
+import org.eclipse.jetty.servlet.FilterHolder;
+import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.security.cert.X509Certificate;
 import java.util.Properties;
 
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.core.Configurable;
 
 import io.confluent.kafka.schemaregistry.rest.resources.CompatibilityResource;
@@ -34,6 +45,8 @@ import io.confluent.kafka.schemaregistry.exceptions.SchemaRegistryException;
 import io.confluent.kafka.schemaregistry.storage.serialization.SchemaRegistrySerializer;
 import io.confluent.rest.Application;
 import io.confluent.rest.RestConfigException;
+import scala.collection.JavaConversions;
+import scala.collection.immutable.Set;
 
 public class SchemaRegistryRestApplication extends Application<SchemaRegistryConfig> {
 
@@ -64,6 +77,69 @@ public class SchemaRegistryRestApplication extends Application<SchemaRegistryCon
     config.register(new SchemasResource(schemaRegistry));
     config.register(new SubjectVersionsResource(schemaRegistry));
     config.register(new CompatibilityResource(schemaRegistry));
+  }
+
+  protected void configurePreResourceHandling(ServletContextHandler context) {
+    Filter filter = new Filter() {
+      private final SimpleAclAuthorizer authorizer = new SimpleAclAuthorizer();
+
+      @Override
+      public void init(FilterConfig filterConfig) throws ServletException {
+        authorizer.configure((ImmutableMap.of("zookeeper.connect", "<zookeeper_connect_url_specify_here>")));
+      }
+
+      @Override
+      public void doFilter(ServletRequest servletRequest,
+                           ServletResponse servletResponse,
+                           FilterChain filterChain) throws IOException, ServletException {
+        // FIXME -> check authorization only for certain URLs
+
+        // FIXME -> extract topic name from URL
+        String topicName = "test";
+        Object certsObj = servletRequest.getAttribute("javax.servlet.request.X509Certificate");
+        if (certsObj instanceof X509Certificate[]) {
+          X509Certificate[] certs = (X509Certificate[]) certsObj;
+          boolean canChangeSchema = false;
+          for (X509Certificate cert : certs) {
+            String user = buildUser(cert);
+            Set<Acl> acls = authorizer.getAcls(Resource.fromString("Topic:" + topicName));
+            for (Acl acl : JavaConversions.setAsJavaSet(acls)) {
+              if (acl.principal().getName().equals(user) && acl.operation().equals(Write$.MODULE$)) {
+                canChangeSchema = true;
+                break;
+              }
+            }
+          }
+
+          if (!canChangeSchema) {
+            String msg = "You are unauthorized to change schema for topic " + topicName;
+            if (servletResponse instanceof HttpServletResponse) {
+              HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+              httpResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED, msg);
+            } else {
+              // it is very unlikely to enter here
+              servletResponse.getOutputStream().println(msg);
+            }
+            return; // do not pass on request processing
+          }
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+      }
+
+      private String buildUser(X509Certificate cert) {
+        // FIXME -> allow customizing user name based on certificate
+        return "test";
+      }
+
+      @Override
+      public void destroy() {
+        authorizer.close();
+      }
+    };
+
+    FilterHolder authorizationFilterHolder = new FilterHolder(filter);
+    context.addFilter(authorizationFilterHolder, "/*", null);
   }
 
   @Override
