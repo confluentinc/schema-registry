36c31eacb42b099ab1d2b3809d927295f20011db
Mateusz Bukowicz <mateusz.bukowicz@nordea.com>
2017-05-26 12:11:12 +0200

    Add an assertion to schema registry client test case
    Add an assertion for the error code in schema registry client test case
    where we test a scenario without correctly set ACLs.

--- core/src/test/java/io/confluent/kafka/schemaregistry/rest/RestApiAuthorizationTest.java
+++ core/src/test/java/io/confluent/kafka/schemaregistry/rest/RestApiAuthorizationTest.java
@@ -5,6 +5,7 @@ import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientExcept
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class RestApiAuthorizationTest extends SSLClusterRequiringAuthorizationTestHarness {
 
@@ -14,6 +15,8 @@ public class RestApiAuthorizationTest extends SSLClusterRequiringAuthorizationTe
     public static final String TEST_SCHEMA_BODY = "{\"schema\": \"{\\\"type\\\": \\\"string\\\"}\"}";
     public static final String EXPECTED_200_MSG = "Response status must be 200.";
     public static final String EXPECTED_401_MSG = "Response status must be 401.";
+    public static final String EXPECTED_REST_CLIENT_EXCEPTION_WITH_401_MSG = "Schema Registry " +
+            "client should throw RestclientException with 401 error code";
 
     public RestApiAuthorizationTest() {
         super(TEST_USER);
@@ -27,19 +30,27 @@ public class RestApiAuthorizationTest extends SSLClusterRequiringAuthorizationTe
     }
 
     @Test
-    public void testAuthorizationWithCorrectCertificateButLackOfACLsUsingHttpClient() throws Exception {
+    public void testAuthorizationWithCorrectCertificateButLackOfACLsUsingHttpClient()
+            throws Exception {
         int statusCode = updateSchemaUsingHttpClient(TEST_TOPIC, TEST_SCHEMA_BODY);
         assertEquals(EXPECTED_401_MSG, 401, statusCode);
     }
 
     @Test
-    public void testAuthorizationWithCorrectCertificateAndACLsUsingSchemaRegistryClient() throws Exception {
+    public void testAuthorizationWithCorrectCertificateAndACLsUsingSchemaRegistryClient()
+            throws Exception {
         addWriteAcl(TEST_USER, TEST_TOPIC);
         updateSchemaRegistryUsingSchemaRegistryClient(TEST_TOPIC, TEST_SCHEMA);
     }
 
-    @Test(expected = RestClientException.class)
-    public void testAuthorizationWithCorrectCertificateButLackOfACLsUsingSchemaRegistryClient() throws Exception {
-        updateSchemaRegistryUsingSchemaRegistryClient(TEST_TOPIC, TEST_SCHEMA);
+    @Test
+    public void testAuthorizationWithCorrectCertificateButLackOfACLsUsingSchemaRegistryClient()
+            throws Exception {
+        try {
+            updateSchemaRegistryUsingSchemaRegistryClient(TEST_TOPIC, TEST_SCHEMA);
+            fail(EXPECTED_REST_CLIENT_EXCEPTION_WITH_401_MSG);
+        } catch (RestClientException ex) {
+            assertEquals(EXPECTED_401_MSG, 401, ex.getErrorCode());
+        }
     }
 }
