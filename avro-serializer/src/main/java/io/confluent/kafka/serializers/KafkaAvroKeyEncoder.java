44176ce8850cad8ab89c3c0c5db11025b2f3429b
smueller18 <mail@stephanmueller.eu>
2017-05-30 09:11:00 +0200

    add KafkaAvroKeyEncoder and KafkaAvroValueEncoder

---
+++ avro-serializer/src/main/java/io/confluent/kafka/serializers/KafkaAvroKeyEncoder.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2017 Confluent Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.confluent.kafka.serializers;
+
+import org.apache.avro.generic.GenericContainer;
+import org.apache.kafka.common.errors.SerializationException;
+
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import kafka.serializer.Encoder;
+import kafka.utils.VerifiableProperties;
+
+
+public class KafkaAvroKeyEncoder extends AbstractKafkaAvroSerializer implements Encoder<Object> {
+
+    public KafkaAvroKeyEncoder(SchemaRegistryClient schemaRegistry) {
+        this.schemaRegistry = schemaRegistry;
+    }
+
+    /**
+     * Constructor used by Kafka producer.
+     */
+    public KafkaAvroKeyEncoder(VerifiableProperties props) {
+        configure(serializerConfig(props));
+    }
+
+    @Override
+    public byte[] toBytes(Object object) {
+        if (object instanceof GenericContainer) {
+            return serializeImpl(getSubjectName(((GenericContainer) object).getSchema().getName(), true), object);
+        } else {
+            throw new SerializationException("Primitive types are not supported yet");
+        }
+    }
+}
