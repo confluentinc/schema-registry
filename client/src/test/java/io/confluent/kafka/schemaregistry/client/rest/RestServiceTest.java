226623cab44fde83f8a0a31bb1636885ae9a94c3
tboldt <tboldt@ca.ibm.com>
2017-09-21 15:38:45 -0400

    Add unit test for basic authentication header.

--- client/src/test/java/io/confluent/kafka/schemaregistry/client/rest/RestServiceTest.java
+++ client/src/test/java/io/confluent/kafka/schemaregistry/client/rest/RestServiceTest.java
@@ -15,10 +15,29 @@
  */
 package io.confluent.kafka.schemaregistry.client.rest;
 
-import org.junit.Test;
-
 import static junit.framework.TestCase.assertEquals;
+import static org.easymock.EasyMock.anyBoolean;
+import static org.easymock.EasyMock.anyInt;
+import static org.easymock.EasyMock.anyObject;
+import static org.easymock.EasyMock.anyString;
+import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.expectLastCall;
+import static org.easymock.EasyMock.verify;
+import static org.powermock.api.easymock.PowerMock.expectNew;
+import static org.powermock.api.easymock.PowerMock.replay;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.easymock.annotation.Mock;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({RestService.class, URL.class})
 public class RestServiceTest {
 
   @Test
@@ -51,5 +70,81 @@ public class RestServiceTest {
     String path = "/some/path";
 
     assertEquals("http://test.com/some/path", RestService.buildRequestUrl(baseUrl, path));
+  }
+
+  @Mock
+  private URL url;
+
+  @Mock
+  private HttpURLConnection httpURLConnection;
+
+  @Mock
+  private InputStream inputStream;
+
+  /*
+   * Test setBasicAuthRequestHeader (private method) indirectly through getAllSubjects.
+   */
+  @Test
+  public void testSetBasicAuthRequestHeader() throws Exception {
+    RestService restService = new RestService("http://localhost:8081");
+
+    expectNew(URL.class, anyString()).andReturn(url).anyTimes();
+
+    expect(url.openConnection()).andReturn(httpURLConnection).anyTimes();
+
+    expect(url.getUserInfo()).andReturn("user:password").anyTimes();
+
+    expect(httpURLConnection.getResponseCode()).andReturn(HttpURLConnection.HTTP_OK).anyTimes();
+
+    // Make sure that the Authorization header is set with the correct value for "user:password"
+    httpURLConnection.setRequestProperty("Authorization", "Basic dXNlcjpwYXNzd29yZA==");
+    expectLastCall().once();
+
+    httpURLConnection.setRequestProperty("Content-Type", "application/vnd.schemaregistry.v1+json");
+    expectLastCall().anyTimes();
+
+    httpURLConnection.disconnect();
+    expectLastCall().anyTimes();
+
+    httpURLConnection.setRequestMethod(anyString());
+    expectLastCall().anyTimes();
+
+    httpURLConnection.setDoInput(anyBoolean());
+    expectLastCall().anyTimes();
+
+    httpURLConnection.setUseCaches(anyBoolean());
+    expectLastCall().anyTimes();
+
+    expect(httpURLConnection.getInputStream()).andReturn(inputStream).anyTimes();
+
+    expect(inputStream.read((byte[]) anyObject(), anyInt(), anyInt()))
+        .andDelegateTo(new InputStream() {
+          @Override
+          public int read() {
+            return 0;
+          }
+
+          @Override
+          public int read(byte[] b, int off, int len) {
+            byte[] json = "[\"abc\"]".getBytes(StandardCharsets.UTF_8);
+            System.arraycopy(json, 0, b, 0, json.length);
+            return json.length;
+          }
+        }).anyTimes();
+
+    inputStream.close();
+    expectLastCall().anyTimes();
+
+    replay(URL.class, url);
+    replay(HttpURLConnection.class, httpURLConnection);
+    replay(InputStream.class, inputStream);
+
+    List<String> allSubjects = restService.getAllSubjects();
+
+    verify(inputStream);
+    verify(httpURLConnection);
+
+    assertEquals(1, allSubjects.size());
+    assertEquals("abc", allSubjects.get(0));
   }
 }
