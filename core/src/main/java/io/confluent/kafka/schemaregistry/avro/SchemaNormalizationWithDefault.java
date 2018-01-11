2e60c642e9cab0e88302182a10798c28d830ca63
Gavrie Philipson <gavrie@philipson.co.il>
2018-01-11 14:39:03 +0200

    Remove unneeded code and rename SchemaNormalization

--- core/src/main/java/io/confluent/kafka/schemaregistry/avro/SchemaNormalizationWithDefault.java
+++ core/src/main/java/io/confluent/kafka/schemaregistry/avro/SchemaNormalizationWithDefault.java
@@ -20,23 +20,21 @@ package io.confluent.kafka.schemaregistry.avro;
 import org.apache.avro.Schema;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
-/** Collection of static methods for generating the cannonical form of
- * schemas (see {@link #toParsingForm}) -- and fingerprints of cannonical
- * forms ({@link #fingerprint}).
+/**
+ * Collection of static methods for generating the canonical form of schemas.
+ *
+ * Code lifted from {@link org.apache.avro.SchemaNormalization}.
  */
-public class SchemaNormalization {
+public class SchemaNormalizationWithDefault {
 
-  private SchemaNormalization() {}
+  private SchemaNormalizationWithDefault() {}
 
   /** Returns "Parsing Canonical Form" of a schema as defined by Avro
-    * spec. */
-  public static String toParsingForm(Schema s) {
+    * spec, with the addition of 'default' field attributes. */
+  public static String toCanonicalForm(Schema s) {
     try {
       Map<String,String> env = new HashMap<String,String>();
       return build(env, s, new StringBuilder()).toString();
@@ -44,65 +42,6 @@ public class SchemaNormalization {
       // Shouldn't happen, b/c StringBuilder can't throw IOException
       throw new RuntimeException(e);
     }
-  }
-
-  /** Returns a fingerprint of a string of bytes.  This string is
-    * presumed to contain a canonical form of a schema.  The
-    * algorithm used to compute the fingerprint is selected by the
-    * argument <i>fpName</i>.  If <i>fpName</i> equals the string
-    * <code>"CRC-64-AVRO"</code>, then the result of {@link #fingerprint64} is
-    * returned in little-endian format.  Otherwise, <i>fpName</i> is
-    * used as an algorithm name for {@link
-    * MessageDigest#getInstance(String)}, which will throw
-    * <code>NoSuchAlgorithmException</code> if it doesn't recognize
-    * the name.
-    * <p> Recommended Avro practice dictiates that
-    * <code>"CRC-64-AVRO"</code> is used for 64-bit fingerprints,
-    * <code>"MD5"</code> is used for 128-bit fingerprints, and
-    * <code>"SHA-256"</code> is used for 256-bit fingerprints. */
-  public static byte[] fingerprint(String fpName, byte[] data)
-    throws NoSuchAlgorithmException
-  {
-    if (fpName.equals("CRC-64-AVRO")) {
-      long fp = fingerprint64(data);
-      byte[] result = new byte[8];
-      for (int i = 0; i < 8; i++) {
-        result[i] = (byte)fp;
-        fp >>= 8;
-      }
-      return result;
-    }
-
-    MessageDigest md = MessageDigest.getInstance(fpName);
-    return md.digest(data);
-  }
-
-  /** Returns the 64-bit Rabin Fingerprint (as recommended in the Avro
-    * spec) of a byte string. */
-  public static long fingerprint64(byte[] data) {
-    long result = EMPTY64;
-    for (byte b: data)
-      result = (result >>> 8) ^ FP64.FP_TABLE[(int)(result ^ b) & 0xff];
-    return result;
-  }
-
-  /** Returns {@link #fingerprint} applied to the parsing canonical form
-    * of the supplied schema. */
-  public static byte[] parsingFingerprint(String fpName, Schema s)
-    throws NoSuchAlgorithmException
-  {
-    try {
-      return fingerprint(fpName, toParsingForm(s).getBytes("UTF-8"));
-    } catch (UnsupportedEncodingException e) { throw new RuntimeException(e); }
-  }
-
-  /** Returns {@link #fingerprint64} applied to the parsing canonical form
-    * of the supplied schema. */
-  public static long parsingFingerprint64(Schema s) {
-    try {
-      return fingerprint64(toParsingForm(s).getBytes("UTF-8"));
-    } catch (UnsupportedEncodingException e)
-      { throw new RuntimeException(e); }
   }
 
   private static Appendable build(Map<String,String> env, Schema s,
@@ -157,20 +96,4 @@ public class SchemaNormalization {
     }
   }
 
-  final static long EMPTY64 = 0xc15d213aa4d7a795L;
-
-  /* An inner class ensures that FP_TABLE initialized only when needed. */
-  private static class FP64 {
-    private static final long[] FP_TABLE = new long[256];
-    static {
-      for (int i = 0; i < 256; i++) {
-        long fp = i;
-        for (int j = 0; j < 8; j++) {
-          long mask = -(fp & 1L);
-          fp = (fp >>> 1) ^ (EMPTY64 & mask);
-        }
-        FP_TABLE[i] = fp;
-      }
-    }
-  }
 }
