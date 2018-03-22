59412e20f657291a1fa87558b2733178a22652bd
Bill Bejeck <bill@confluent.io>
2018-03-22 13:33:21 -0400

    changes per comments

--- core/src/test/java/io/confluent/kafka/schemaregistry/RestApp.java
+++ core/src/test/java/io/confluent/kafka/schemaregistry/RestApp.java
@@ -72,6 +72,12 @@ public class RestApp {
     }
   }
 
+  /**
+   * This method must be called before calling {@code RestApp.start()}
+   * for the additional properties to take affect.
+   *
+   * @param props the additional properties to set
+   */
   public void addConfigs(Properties props) {
     prop.putAll(props);
   }
