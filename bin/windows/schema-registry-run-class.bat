89f5600a40552c7b32e2a41cb40e24e2acd81331
Max Zheng <maxzheng.os@gmail.com>
2018-12-21 23:12:27 +0000

    Revert non-core to use Apache as it was changed accidentally

--- bin/windows/schema-registry-run-class.bat
+++ bin/windows/schema-registry-run-class.bat
@@ -2,15 +2,17 @@
 rem
 rem Copyright 2018 Confluent Inc.
 rem
-rem Licensed under the Confluent Community License; you may not use this file
-rem except in compliance with the License.  You may obtain a copy of the License at
+rem Licensed under the Apache License, Version 2.0 (the "License");
+rem you may not use this file except in compliance with the License.
+rem You may obtain a copy of the License at
 rem
-rem http://www.confluent.io/confluent-community-license
+rem http://www.apache.org/licenses/LICENSE-2.0
 rem
 rem Unless required by applicable law or agreed to in writing, software
-rem distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-rem WARRANTIES OF ANY KIND, either express or implied.  See the License for the
-rem specific language governing permissions and limitations under the License.
+rem distributed under the License is distributed on an "AS IS" BASIS,
+rem WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+rem See the License for the specific language governing permissions and
+rem limitations under the License.
 rem
 
 
