e355be01d7236929a94a38000fc24635e42acb03
Robert Yokota <rayokota@gmail.com>
2019-01-31 18:01:49 -0800

    MINOR: do not try to validate schema deletes

--- core/src/main/java/io/confluent/kafka/schemaregistry/storage/KafkaStoreMessageHandler.java
+++ core/src/main/java/io/confluent/kafka/schemaregistry/storage/KafkaStoreMessageHandler.java
@@ -43,7 +43,7 @@ public class KafkaStoreMessageHandler
   public boolean validateUpdate(SchemaRegistryKey key, SchemaRegistryValue value) {
     if (key.getKeyType() == SchemaRegistryKeyType.SCHEMA) {
       SchemaValue schemaObj = (SchemaValue) value;
-      if (schemaObj != null) {
+      if (schemaObj != null && !schemaObj.isDeleted()) {
         SchemaKey oldKey = schemaRegistry.guidToSchemaKey.get(schemaObj.getId());
         if (oldKey != null) {
           SchemaValue oldSchema;
