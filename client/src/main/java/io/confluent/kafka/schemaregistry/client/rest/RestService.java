b8f8ca5e2755095df61b322d2339ad8b563dbad3
c.piotre <c.piotre@gmail.com>
2019-03-01 23:30:14 +0100

    Use try-with resources to avoid NPE in RestService
    When client requests schema for a topic name containing invalid
    characters (e.g. space character), the server responds with
    4XX status code,  but there is no content in the error stream.
    This leads to NullPointerException being thrown instead of expected
    RestException.
    
    Resolves issue #1037.

--- client/src/main/java/io/confluent/kafka/schemaregistry/client/rest/RestService.java
+++ client/src/main/java/io/confluent/kafka/schemaregistry/client/rest/RestService.java
@@ -190,18 +190,12 @@ public class RestService {
 
       if (requestBodyData != null) {
         connection.setDoOutput(true);
-        OutputStream os = null;
-        try {
-          os = connection.getOutputStream();
+        try (OutputStream os = connection.getOutputStream()) {
           os.write(requestBodyData);
           os.flush();
         } catch (IOException e) {
           log.error("Failed to send HTTP request to endpoint: " + url, e);
           throw e;
-        } finally {
-          if (os != null) {
-            os.close();
-          }
         }
       }
 
@@ -214,14 +208,12 @@ public class RestService {
       } else if (responseCode == HttpURLConnection.HTTP_NO_CONTENT) {
         return null;
       } else {
-        InputStream es = connection.getErrorStream();
         ErrorMessage errorMessage;
-        try {
+        try (InputStream es = connection.getErrorStream()) {
           errorMessage = jsonDeserializer.readValue(es, ErrorMessage.class);
         } catch (JsonProcessingException e) {
           errorMessage = new ErrorMessage(JSON_PARSE_ERROR_CODE, e.getMessage());
         }
-        es.close();
         throw new RestClientException(errorMessage.getMessage(), responseCode,
                                       errorMessage.getErrorCode());
       }
