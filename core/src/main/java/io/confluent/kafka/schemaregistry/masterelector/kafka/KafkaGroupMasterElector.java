6956803c1000c1b9f9e3cb3863366417352dc0ec
Robert Yokota <ryokota@confluent.io>
2020-01-08 10:37:36 -0800

    MINOR: fix createChannelBuilder call (#1284)

--- core/src/main/java/io/confluent/kafka/schemaregistry/masterelector/kafka/KafkaGroupMasterElector.java
+++ core/src/main/java/io/confluent/kafka/schemaregistry/masterelector/kafka/KafkaGroupMasterElector.java
@@ -123,7 +123,8 @@ public class KafkaGroupMasterElector implements MasterElector, SchemaRegistryReb
       this.metadata.bootstrap(addresses);
       String metricGrpPrefix = "kafka.schema.registry";
 
-      ChannelBuilder channelBuilder = ClientUtils.createChannelBuilder(clientConfig, time);
+      ChannelBuilder channelBuilder = ClientUtils.createChannelBuilder(
+          clientConfig, time, new LogContext());
       long maxIdleMs = clientConfig.getLong(CommonClientConfigs.CONNECTIONS_MAX_IDLE_MS_CONFIG);
 
       NetworkClient netClient = new NetworkClient(
