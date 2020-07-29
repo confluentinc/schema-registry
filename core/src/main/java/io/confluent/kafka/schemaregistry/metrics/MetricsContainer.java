956231fcc3af33669f5e3ad19d17167c842935c6
Dragos Misca <dragosvictor@users.noreply.github.com>
2020-07-28 17:36:06 -0700

    DG-715: Change telemetry cluster label names

--- core/src/main/java/io/confluent/kafka/schemaregistry/metrics/MetricsContainer.java
+++ core/src/main/java/io/confluent/kafka/schemaregistry/metrics/MetricsContainer.java
@@ -44,8 +44,8 @@ public class MetricsContainer {
   public static final String JMX_PREFIX = "kafka.schema.registry";
 
   public static final String RESOURCE_LABEL_PREFIX = "resource.";
-  public static final String RESOURCE_LABEL_CLUSTER_ID = RESOURCE_LABEL_PREFIX + "cluster.id";
-  public static final String RESOURCE_LABEL_GROUP_ID = RESOURCE_LABEL_PREFIX + "group.id";
+  public static final String RESOURCE_LABEL_CLUSTER_ID = RESOURCE_LABEL_PREFIX + "kafka.cluster.id";
+  public static final String RESOURCE_LABEL_GROUP_ID = RESOURCE_LABEL_PREFIX + "cluster.id";
   public static final String RESOURCE_LABEL_TYPE = RESOURCE_LABEL_PREFIX + "type";
   public static final String RESOURCE_LABEL_VERSION = RESOURCE_LABEL_PREFIX + "version";
   public static final String RESOURCE_LABEL_COMMIT_ID = RESOURCE_LABEL_PREFIX + "commit.id";
