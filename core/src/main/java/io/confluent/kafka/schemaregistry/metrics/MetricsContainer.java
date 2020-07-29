f7dc89dc5cfca1e3a5f4afb1fb8e5e5e519328cd
Dragos Misca <dragosvictor@users.noreply.github.com>
2020-07-28 17:50:28 -0700

    Add extra group ID label

--- core/src/main/java/io/confluent/kafka/schemaregistry/metrics/MetricsContainer.java
+++ core/src/main/java/io/confluent/kafka/schemaregistry/metrics/MetricsContainer.java
@@ -47,6 +47,7 @@ public class MetricsContainer {
   public static final String RESOURCE_LABEL_KAFKA_CLUSTER_ID =
           RESOURCE_LABEL_PREFIX + "kafka.cluster.id";
   public static final String RESOURCE_LABEL_CLUSTER_ID = RESOURCE_LABEL_PREFIX + "cluster.id";
+  public static final String RESOURCE_LABEL_GROUP_ID = RESOURCE_LABEL_PREFIX + "group.id";
   public static final String RESOURCE_LABEL_TYPE = RESOURCE_LABEL_PREFIX + "type";
   public static final String RESOURCE_LABEL_VERSION = RESOURCE_LABEL_PREFIX + "version";
   public static final String RESOURCE_LABEL_COMMIT_ID = RESOURCE_LABEL_PREFIX + "commit.id";
@@ -221,12 +222,14 @@ public class MetricsContainer {
 
   private static MetricsContext getMetricsContext(SchemaRegistryConfig config,
                                                   String kafkaClusterId) {
+    String srGroupId = config.getString(SchemaRegistryConfig.SCHEMAREGISTRY_GROUP_ID_CONFIG);
+
     Map<String, Object> metadata =
             config.originalsWithPrefix(CommonClientConfigs.METRICS_CONTEXT_PREFIX);
 
     metadata.put(RESOURCE_LABEL_KAFKA_CLUSTER_ID, kafkaClusterId);
-    metadata.put(RESOURCE_LABEL_CLUSTER_ID,
-            config.getString(SchemaRegistryConfig.SCHEMAREGISTRY_GROUP_ID_CONFIG));
+    metadata.put(RESOURCE_LABEL_CLUSTER_ID, srGroupId);
+    metadata.put(RESOURCE_LABEL_GROUP_ID, srGroupId);
     metadata.put(RESOURCE_LABEL_TYPE,  "schema_registry");
     metadata.put(RESOURCE_LABEL_VERSION, AppInfoParser.getVersion());
     metadata.put(RESOURCE_LABEL_COMMIT_ID, AppInfoParser.getCommitId());
