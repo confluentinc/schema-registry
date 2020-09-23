dae3a4bc164d52fbf07662a986b9abef3afc995e
Xiaoya Li <xli@confluent.io>
2020-09-23 14:46:11 -0700

    style fix

--- avro-converter/src/main/java/io/confluent/connect/avro/AvroData.java
+++ avro-converter/src/main/java/io/confluent/connect/avro/AvroData.java
@@ -55,7 +55,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.IdentityHashMap;
 import java.util.Iterator;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
