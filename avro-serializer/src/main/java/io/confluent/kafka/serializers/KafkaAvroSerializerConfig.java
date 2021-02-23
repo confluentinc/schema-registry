cb673a0ac8ccdcd6613cc7c4609d0af3fea0be11
Emmanuel <emmanuel.brard@getyourguide.com>
2021-02-23 18:47:32 +0100

    Add logical type converter to Kafka avro serializer/deserializer (#1762) (#1763)
    * Add logical type converter to Kafka avro serializer/deserializer (#1762)
    
    * Fix test
    
    * Trigger tests
    
    * Add all available logical type converters to Kafka avro serializer/deserializer (#1762)
    
    * Renamed getAvroDataWithConverters to addLogicalTypeConversion and made it static
    
    * Remove addLogicalTypeConversion from Specific and Reflect
    
    * Remove unused classes
    
    * Fix formatting
    
    Co-authored-by: emmanuel <emmanuel@getyourguide.com>
    Co-authored-by: Robert Yokota <ryokota@confluent.io>
    Co-authored-by: Robert Yokota <rayokota@gmail.com>

--- avro-serializer/src/main/java/io/confluent/kafka/serializers/KafkaAvroSerializerConfig.java
+++ avro-serializer/src/main/java/io/confluent/kafka/serializers/KafkaAvroSerializerConfig.java
@@ -26,6 +26,11 @@ public class KafkaAvroSerializerConfig extends AbstractKafkaSchemaSerDeConfig {
   public static final boolean AVRO_REFLECTION_ALLOW_NULL_DEFAULT = false;
   public static final String AVRO_REFLECTION_ALLOW_NULL_DOC =
       "If true, allows null field values used in ReflectionAvroSerializer";
+  public static final String AVRO_USE_LOGICAL_TYPE_CONVERTERS_CONFIG =
+          "avro.use.logical.type.converters";
+  public static final boolean AVRO_USE_LOGICAL_TYPE_CONVERTERS_DEFAULT = false;
+  public static final String AVRO_USE_LOGICAL_TYPE_CONVERTERS_DOC =
+          "If true, use logical type converter in generic record";
 
   public static final String AVRO_REMOVE_JAVA_PROPS_CONFIG = "avro.remove.java.properties";
   public static final boolean AVRO_REMOVE_JAVA_PROPS_DEFAULT = false;
@@ -39,6 +44,9 @@ public class KafkaAvroSerializerConfig extends AbstractKafkaSchemaSerDeConfig {
         .define(AVRO_REFLECTION_ALLOW_NULL_CONFIG, ConfigDef.Type.BOOLEAN,
             AVRO_REFLECTION_ALLOW_NULL_DEFAULT, ConfigDef.Importance.MEDIUM,
             AVRO_REFLECTION_ALLOW_NULL_DOC)
+        .define(AVRO_USE_LOGICAL_TYPE_CONVERTERS_CONFIG, ConfigDef.Type.BOOLEAN,
+            AVRO_USE_LOGICAL_TYPE_CONVERTERS_DEFAULT, ConfigDef.Importance.MEDIUM,
+            AVRO_USE_LOGICAL_TYPE_CONVERTERS_DOC)
         .define(AVRO_REMOVE_JAVA_PROPS_CONFIG, ConfigDef.Type.BOOLEAN,
             AVRO_REMOVE_JAVA_PROPS_DEFAULT, ConfigDef.Importance.MEDIUM,
             AVRO_REMOVE_JAVA_PROPS_DOC);
