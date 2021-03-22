39edd8cc68f44104ca3c743b84ef62f6263b4b82
Robert Yokota <rayokota@gmail.com>
2021-03-22 16:23:41 -0700

    MINOR synchronize reset() method in client

--- client/src/main/java/io/confluent/kafka/schemaregistry/client/CachedSchemaRegistryClient.java
+++ client/src/main/java/io/confluent/kafka/schemaregistry/client/CachedSchemaRegistryClient.java
@@ -388,7 +388,7 @@ public class CachedSchemaRegistryClient implements SchemaRegistryClient {
   }
 
   @Override
-  public void reset() {
+  public synchronized void reset() {
     schemaCache.clear();
     idCache.clear();
     versionCache.clear();
