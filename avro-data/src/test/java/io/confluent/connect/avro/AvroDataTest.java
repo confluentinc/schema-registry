e8fb2c30a35e8c88d97af42cdfdcf074b5285448
Robert Yokota <ryokota@confluent.io>
2021-08-20 08:28:40 -0700

    DGS-2018 Ensure names in Avro union are scrubbed (#2012)

--- avro-data/src/test/java/io/confluent/connect/avro/AvroDataTest.java
+++ avro-data/src/test/java/io/confluent/connect/avro/AvroDataTest.java
@@ -292,6 +292,44 @@ public class AvroDataTest {
   }
 
   @Test
+  public void testFromConnectOptionalRecordWithInvalidName() {
+    AvroDataConfig avroDataConfig = new AvroDataConfig.Builder()
+        .with(AvroDataConfig.SCRUB_INVALID_NAMES_CONFIG, true)
+        .with(AvroDataConfig.CONNECT_META_DATA_CONFIG, false)
+        .build();
+    AvroData avroData = new AvroData(avroDataConfig);
+    Schema schema = SchemaBuilder.struct()
+        .name("invalid record-name")
+        .optional()
+        .field("invalid field-name", SchemaBuilder.bool().optional().defaultValue(null).build())
+        .build();
+    org.apache.avro.Schema avroSchema = avroData.fromConnectSchema(schema);
+    org.apache.avro.Schema recordSchema = org.apache.avro.SchemaBuilder.builder()
+        .record("invalid_record_name").fields()
+        .name("invalid_field_name").type(org.apache.avro.SchemaBuilder.builder()
+            .unionOf().nullType().and().booleanType().endUnion()).withDefault(null)
+        .endRecord();
+    org.apache.avro.Schema expectedAvroSchema = org.apache.avro.SchemaBuilder.builder()
+        .unionOf().nullType().and()
+        .record("invalid_record_name").fields()
+        .name("invalid_field_name").type(org.apache.avro.SchemaBuilder.builder()
+            .unionOf().nullType().and().booleanType().endUnion()).withDefault(null)
+        .endRecord()
+        .endUnion();
+
+    assertEquals(expectedAvroSchema, avroSchema);
+
+    Struct struct = new Struct(schema)
+        .put("invalid field-name", true);
+    Object convertedRecord = avroData.fromConnectData(schema, struct);
+    org.apache.avro.generic.GenericRecord avroRecord = new org.apache.avro.generic.GenericRecordBuilder(recordSchema)
+        .set("invalid_field_name", true)
+        .build();
+
+    assertEquals(avroRecord, convertedRecord);
+  }
+
+  @Test
   public void testNameScrubbing() {
     assertEquals("abc_2B____", AvroData.doScrubName("abc+-.*_"));
     assertEquals("abc_def", AvroData.doScrubName("abc-def"));
