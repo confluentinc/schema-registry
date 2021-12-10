0c5ffbbe7a9a0c16b8fe9ea031371c8ec42deff1
Robert Macaulay <robert.macaulay@gmail.com>
2021-12-10 11:41:50 -0600

    Fixes #1930: support Jakarta EE 9 by eliminating dependencies on jersey-common and jakarta.ws.rs in schema-registry-client. (#1933) (#2039) (#2100)
    * Fixes #1930: removing dependencies on jersey-common and jakarta.ws.rs from schema-registry-client.
    * Adding simplfied UriBuiler as replacement, with support for basic urlencoding for query
      parameters (based on java.net.UrlEncode) and path segments (RFC 3986)
    
    * removing reference to Respone.Family class from javax.ws.rs in module
      schema-serializer
    
    * Update UriBuilder.java
    
    * Update pom.xml
    
    Co-authored-by: Robert Yokota <rayokota@gmail.com>
    
    Co-authored-by: Robert Yokota <rayokota@gmail.com>
    
    Co-authored-by: Frode Carlsen <frode.carlsen@soprasteria.com>
    Co-authored-by: Robert Yokota <rayokota@gmail.com>

---
+++ client/src/test/java/io/confluent/kafka/schemaregistry/client/rest/UriBuilderTest.java
@@ -0,0 +1,108 @@
+package io.confluent.kafka.schemaregistry.client.rest;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Test;
+
+import io.confluent.kafka.schemaregistry.client.rest.UriBuilder.UriPercentEncoder;
+
+public class UriBuilderTest {
+  private static final Charset UTF8 = Charset.forName("UTF-8");
+
+  @Test
+  public void findTemplateNamesInPath() {
+    List<String> templateNames = UriBuilder
+        .findNamesInTemplate("https://some.host/{subject}/and/{version}/resource");
+    assertEquals(templateNames, Arrays.asList("{subject}", "{version}"));
+  }
+
+  @Test
+  public void encodeCommonPathSegmentChars() throws Exception {
+    assertEquals("%0A", UriPercentEncoder.encode("\n", UTF8));
+    assertEquals("%22", UriPercentEncoder.encode("\"", UTF8));
+    assertEquals("%25", UriPercentEncoder.encode("%", UTF8));
+    assertEquals("%3C", UriPercentEncoder.encode("<", UTF8));
+    assertEquals("%3E", UriPercentEncoder.encode(">", UTF8));
+    assertEquals("%5C", UriPercentEncoder.encode("\\", UTF8));
+    assertEquals("%5E", UriPercentEncoder.encode("^", UTF8));
+    assertEquals("%60", UriPercentEncoder.encode("`", UTF8));
+    assertEquals("%7B", UriPercentEncoder.encode("{", UTF8));
+    assertEquals("%7C", UriPercentEncoder.encode("|", UTF8));
+    assertEquals("%7D", UriPercentEncoder.encode("}", UTF8));
+    assertEquals("%3F", UriPercentEncoder.encode("?", UTF8));
+
+    assertEquals("foo%20bar%3F", UriPercentEncoder.encode("foo bar?", UTF8));
+  }
+
+  @Test
+  public void doNotEncodeAllowedPathSegmentChars() throws Exception {
+    for (char c : UriPercentEncoder.CHARS_UNENCODE.toCharArray()) {
+      String s = Character.toString(c);
+      assertEquals(s, UriPercentEncoder.encode(s, UTF8));
+    }
+  }
+
+  @Test
+  public void verifyPathSegmentPercentEncodingOfCharactersHandledDifferentlyInLegacyRFCs()
+      throws Exception {
+    assertEquals("+", UriPercentEncoder.encode("+", UTF8)); // do not encode subdelim + (RFC 3986)
+    assertEquals("%20", UriPercentEncoder.encode(" ", UTF8)); // do percent encode space (is supported in both path and query segments)
+    assertEquals("~", UriPercentEncoder.encode("~", UTF8)); // do not encode subdelim ~ (RFC 3986)
+    assertEquals("*", UriPercentEncoder.encode("*", UTF8)); // do not encode subdelim * (RFC 3986)
+  }
+
+  @Test
+  public void buildPathTemplateParameters() throws Exception {
+    UriBuilder uriBuilder = UriBuilder.fromPath("/some/site/{part}");
+    URI uri = uriBuilder.build("mypart");
+    assertEquals(uri, new URI("/some/site/mypart"));
+  }
+
+  @Test
+  public void buildEncodedPathTemplateParameters() throws Exception {
+    assertEquals(new URI("/some/site/my%20part"),
+        UriBuilder.fromPath("/some/site/{part}").build("my part"));
+
+    assertEquals(new URI("/some/site/my_part"),
+        UriBuilder.fromPath("/some/site/{part}").build("my_part"));
+
+    assertEquals(new URI("/some/site/my%3Cpart/more"),
+        UriBuilder.fromPath("/some/site/{part}/more").build("my<part"));
+
+    assertEquals(new URI("/some/site/my%2Fpart/more"),
+        UriBuilder.fromPath("/some/site/{part}/more").build("my/part"));
+  }
+
+  @Test
+  public void buildPathQueryParameters() throws Exception {
+    UriBuilder uriBuilder = UriBuilder.fromPath("/some/site");
+    uriBuilder.queryParam("first", "1");
+    uriBuilder.queryParam("second", 2);
+    assertEquals(uriBuilder.build(), new URI("/some/site?first=1&second=2"));
+  }
+
+  @Test
+  public void testApi() throws Exception {
+    String subject = "testTopic";
+    int version = 2;
+    UriBuilder builder = UriBuilder.fromPath("/subjects/{subject}/versions/{version}")
+        .queryParam("deleted", true);
+    String path = builder.build(subject, version).toString();
+
+    assertEquals(path, "/subjects/testTopic/versions/2?deleted=true");
+  }
+
+  @Test
+  public void checkEncodingOfSomeStandardSubjectTopicNames() throws Exception {
+    UriBuilder builder = UriBuilder.fromPath("/subjects/{subject}");
+    assertEquals("/subjects/my1.topic.name", builder.build("my1.topic.name").toString());
+    assertEquals("/subjects/another-topic-name", builder.build("another-topic-name").toString());
+    assertEquals("/subjects/My_topic_name", builder.build("My_topic_name").toString());
+  }
+
+}
