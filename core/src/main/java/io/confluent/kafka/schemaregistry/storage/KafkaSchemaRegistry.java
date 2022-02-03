0cad023681627d1441d55fc7c205d0bea689314d
Anshul-Goyal <agoyal@confluent.io>
2022-02-03 18:39:48 +0530

    DGS-2929: basic linear time getLatestVersion impl

--- core/src/main/java/io/confluent/kafka/schemaregistry/storage/KafkaSchemaRegistry.java
+++ core/src/main/java/io/confluent/kafka/schemaregistry/storage/KafkaSchemaRegistry.java
@@ -1374,9 +1374,27 @@ public class KafkaSchemaRegistry implements SchemaRegistry, LeaderAwareSchemaReg
   @Override
   public Schema getLatestVersion(String subject) throws SchemaRegistryException {
     try (CloseableIterator<SchemaRegistryValue> allVersions = allVersions(subject, false)) {
-      List<Schema> sortedVersions = sortSchemasByVersion(allVersions, false);
-      return sortedVersions.size() > 0 ? sortedVersions.get(sortedVersions.size() - 1) : null;
+      return getLatestVersionFromSubjectSchemas(allVersions, false);
     }
+  }
+
+  private Schema getLatestVersionFromSubjectSchemas(CloseableIterator<SchemaRegistryValue> schemas,
+                                                    boolean returnDeletedSchema) {
+    int latestVersionId = -1;
+    SchemaValue latestSchemaValue = null;
+
+    while (schemas.hasNext()) {
+      SchemaValue schemaValue = (SchemaValue) schemas.next();
+      if (!returnDeletedSchema && schemaValue.isDeleted()) {
+        continue;
+      }
+      if (schemaValue.getVersion() > latestVersionId) {
+        latestVersionId = schemaValue.getVersion();
+        latestSchemaValue = schemaValue;
+      }
+    }
+
+    return latestSchemaValue != null ? getSchemaEntityFromSchemaValue(latestSchemaValue) : null;
   }
 
   private CloseableIterator<SchemaRegistryValue> allVersions(
