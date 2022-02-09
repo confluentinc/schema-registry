fed61ef5ed04b9238a1a859f944f1a0d8fb0c780
Anshul-Goyal <agoyal@confluent.io>
2022-02-09 17:18:29 +0530

    DGS-2975: unit tests for protobuf schema parsing

--- protobuf-provider/src/test/java/io/confluent/kafka/schemaregistry/protobuf/ProtobufSchemaTest.java
+++ protobuf-provider/src/test/java/io/confluent/kafka/schemaregistry/protobuf/ProtobufSchemaTest.java
@@ -24,6 +24,7 @@ import com.google.protobuf.Descriptors.Descriptor;
 import com.google.protobuf.Descriptors.FieldDescriptor;
 import com.google.protobuf.DynamicMessage;
 import com.squareup.wire.schema.internal.parser.ProtoFileElement;
+import io.confluent.kafka.schemaregistry.ParsedSchema;
 import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaReference;
 import io.confluent.kafka.schemaregistry.protobuf.dynamic.DynamicSchema;
 import io.confluent.kafka.schemaregistry.protobuf.dynamic.MessageDefinition;
@@ -35,6 +36,7 @@ import org.junit.Test;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 
 import io.confluent.kafka.schemaregistry.CompatibilityLevel;
 import io.confluent.kafka.schemaregistry.protobuf.diff.ResourceLoader;
@@ -42,6 +44,7 @@ import io.confluent.kafka.schemaregistry.protobuf.diff.ResourceLoader;
 import static io.confluent.kafka.schemaregistry.protobuf.ProtobufSchema.PROTO3;
 import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -966,6 +969,39 @@ public class ProtobufSchemaTest {
     assertEquals(enumAfterMessageSchemaString, enumBeforeMessageSchema.canonicalString());
     assertEquals(enumAfterMessageSchemaString,
         new ProtobufSchema(enumBeforeMessageSchema.toDescriptor()).canonicalString());
+  }
+
+  @Test
+  public void testParseSchema() {
+    ProtobufSchemaProvider protobufSchemaProvider = new ProtobufSchemaProvider();
+    ParsedSchema parsedSchema = protobufSchemaProvider.parseSchemaOrElseThrow(recordSchemaString,
+            new ArrayList<>(), false);
+
+    assertNotNull(parsedSchema);
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testParseSchemaThrowException() {
+    String invalidSchemaString = "syntax = \"proto3\";\n"
+            + "\n"
+            + "option java_package = \"io.confluent.kafka.serializers.protobuf.test\";\n"
+            + "option java_outer_classname = \"TestMessageProtos\";\n"
+            + "\n"
+            + "import \"google/protobuf/descriptor.proto\";\n"
+            + "\n"
+            + "message TestMessage {\n"
+            + "    string test_string = 1 [json_name = \"test_str\"];\n"
+            + "    int32 test_int32 = 8.01;\n"
+            + "}\n";
+
+    ProtobufSchemaProvider protobufSchemaProvider = new ProtobufSchemaProvider();
+    Optional<ParsedSchema> parsedSchema = protobufSchemaProvider.parseSchema(invalidSchemaString,
+            new ArrayList<>(), false);
+    assertFalse(parsedSchema.isPresent());
+
+    //throws exception
+    protobufSchemaProvider.parseSchemaOrElseThrow(invalidSchemaString,
+            new ArrayList<>(), false);
   }
 
   private static JsonNode jsonTree(String jsonData) {
