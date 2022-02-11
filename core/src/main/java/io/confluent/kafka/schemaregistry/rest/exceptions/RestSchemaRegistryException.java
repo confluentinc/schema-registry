f72ef98822524a974cba4e0318e649fef895a4bb
Dragos Misca <dragosvictor@users.noreply.github.com>
2022-02-11 14:43:11 -0800

    Flush out mismatched error codes

--- core/src/main/java/io/confluent/kafka/schemaregistry/rest/exceptions/RestSchemaRegistryException.java
+++ core/src/main/java/io/confluent/kafka/schemaregistry/rest/exceptions/RestSchemaRegistryException.java
@@ -15,7 +15,21 @@
 
 package io.confluent.kafka.schemaregistry.rest.exceptions;
 
+import io.confluent.kafka.schemaregistry.exceptions.IdDoesNotMatchException;
+import io.confluent.kafka.schemaregistry.exceptions.IdGenerationException;
+import io.confluent.kafka.schemaregistry.exceptions.IncompatibleSchemaException;
+import io.confluent.kafka.schemaregistry.exceptions.InvalidSchemaException;
+import io.confluent.kafka.schemaregistry.exceptions.InvalidVersionException;
+import io.confluent.kafka.schemaregistry.exceptions.OperationNotPermittedException;
+import io.confluent.kafka.schemaregistry.exceptions.ReferenceExistsException;
+import io.confluent.kafka.schemaregistry.exceptions.SchemaVersionNotSoftDeletedException;
+import io.confluent.kafka.schemaregistry.exceptions.SubjectNotSoftDeletedException;
 import io.confluent.rest.exceptions.RestServerErrorException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * Indicates some error while performing a schema registry operation
@@ -24,11 +38,20 @@ public class RestSchemaRegistryException extends RestServerErrorException {
 
   private static final int ERROR_CODE = RestServerErrorException.DEFAULT_ERROR_CODE;
 
+  private static final Logger LOG = LoggerFactory.getLogger(RestSchemaRegistryException.class);
+
+  private Collection<Class> forbiddenClasses =
+          Arrays.asList(IdDoesNotMatchException.class, IdGenerationException.class, IncompatibleSchemaException.class, InvalidSchemaException.class, InvalidVersionException.class, OperationNotPermittedException.class, ReferenceExistsException.class, SchemaVersionNotSoftDeletedException.class, SubjectNotSoftDeletedException.class);
+
   public RestSchemaRegistryException(String message) {
     super(message, ERROR_CODE);
   }
 
   public RestSchemaRegistryException(String message, Throwable cause) {
     super(message, ERROR_CODE, cause);
+    if (forbiddenClasses.stream().anyMatch(clazz -> clazz.isInstance(cause))) {
+      final String msg = "DMISCA Invalid Error: " + cause;
+      LOG.error(msg, new Exception(msg, cause));
+    }
   }
 }
