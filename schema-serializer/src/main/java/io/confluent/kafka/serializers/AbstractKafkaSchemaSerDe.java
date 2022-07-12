ed96278798f90b661013d4c802a724df7ccff79d
Robert Yokota <rayokota@gmail.com>
2022-07-12 14:43:00 -0700

    DGS-4172 Bound size of Avro datumReader/Writer caches (#2331)

--- schema-serializer/src/main/java/io/confluent/kafka/serializers/AbstractKafkaSchemaSerDe.java
+++ schema-serializer/src/main/java/io/confluent/kafka/serializers/AbstractKafkaSchemaSerDe.java
@@ -50,7 +50,7 @@ public abstract class AbstractKafkaSchemaSerDe {
 
   protected static final byte MAGIC_BYTE = 0x0;
   protected static final int idSize = 4;
-  private static int DEFAULT_CACHE_CAPACITY = 1000;
+  protected static final int DEFAULT_CACHE_CAPACITY = 1000;
 
   protected SchemaRegistryClient schemaRegistry;
   protected Object keySubjectNameStrategy = new TopicNameStrategy();
