ff32a60bcdb00ea49babc1e3f8986cb65c049615
Dragos Misca <dragosvictor@users.noreply.github.com>
2022-10-18 21:10:36 -0700

    MINOR: Fix OpenAPI samples (#2404)

--- core/src/main/java/io/confluent/kafka/schemaregistry/rest/resources/ConfigResource.java
+++ core/src/main/java/io/confluent/kafka/schemaregistry/rest/resources/ConfigResource.java
@@ -34,6 +34,7 @@ import io.confluent.kafka.schemaregistry.utils.QualifiedSubject;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -252,7 +253,8 @@ public class ConfigResource {
           responses = {
             @ApiResponse(responseCode = "200",
               description = "Operation succeeded. Returns old global compatibility level.",
-              content = @Content(schema = @Schema(implementation = CompatibilityLevel.class))),
+              content = @Content(schema = @Schema(implementation = CompatibilityLevel.class),
+                      examples = {@ExampleObject(value = "FULL_TRANSITIVE")})),
             @ApiResponse(responseCode = "500",
               description = "Internal Server Error. "
                           + "Error code 50001 indicates a failure in the backend data store.",
