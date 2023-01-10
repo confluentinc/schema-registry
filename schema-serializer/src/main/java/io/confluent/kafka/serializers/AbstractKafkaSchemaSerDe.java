d47fbf42a65b6a72ee40f4186faba2cc639d503c
Robert Yokota <rayokota@gmail.com>
2023-01-10 13:53:16 -0800

    Fix checkstyle

--- schema-serializer/src/main/java/io/confluent/kafka/serializers/AbstractKafkaSchemaSerDe.java
+++ schema-serializer/src/main/java/io/confluent/kafka/serializers/AbstractKafkaSchemaSerDe.java
@@ -77,7 +77,6 @@ import org.apache.kafka.common.header.Headers;
 import org.apache.kafka.common.utils.Utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import scala.concurrent.impl.FutureConvertersImpl.P;
 
 /**
  * Common fields and helper methods for both the serializer and the deserializer.
