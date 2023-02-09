7cc244310e54325cad65ffb685b4f52ae051c122
Robert Yokota <rayokota@gmail.com>
2023-02-08 20:10:47 -0700

    DGS-6033 Allow multiple rules of the same type (#2525)
    * DGS-6033 Allow multiple rules of the same type
    
    * Add comment
    
    * Fix checkstyle
    
    * Fix checkstyle
    
    * Minor fix
    
    * Add test for double encryption
    
    * Incorporate review feedback

--- client-encryption-azure/src/test/java/io/confluent/kafka/schemaregistry/encryption/azure/AzureFieldEncryptionProperties.java
+++ client-encryption-azure/src/test/java/io/confluent/kafka/schemaregistry/encryption/azure/AzureFieldEncryptionProperties.java
@@ -14,7 +14,6 @@
  */
 package io.confluent.kafka.schemaregistry.encryption.azure;
 
-import static io.confluent.kafka.schemaregistry.encryption.FieldEncryptionExecutor.DEFAULT_KMS_KEY_ID;
 import static io.confluent.kafka.schemaregistry.encryption.FieldEncryptionExecutor.EMPTY_AAD;
 import static io.confluent.kafka.schemaregistry.encryption.FieldEncryptionExecutor.TEST_CLIENT;
 import static org.mockito.ArgumentMatchers.any;
@@ -48,10 +47,14 @@ public class AzureFieldEncryptionProperties implements FieldEncryptionProperties
     props.put(AbstractKafkaSchemaSerDeConfig.AUTO_REGISTER_SCHEMAS, "false");
     props.put(AbstractKafkaSchemaSerDeConfig.USE_LATEST_VERSION, "true");
     props.put(AbstractKafkaSchemaSerDeConfig.LATEST_CACHE_TTL, "60");
-    props.put(AbstractKafkaSchemaSerDeConfig.RULE_EXECUTORS, "exec");
-    props.put(AbstractKafkaSchemaSerDeConfig.RULE_EXECUTORS + ".exec.class",
+    props.put(AbstractKafkaSchemaSerDeConfig.RULE_EXECUTORS, "rule1,rule2");
+    props.put(AbstractKafkaSchemaSerDeConfig.RULE_EXECUTORS + ".rule1.class",
         AzureFieldEncryptionExecutor.class.getName());
-    props.put(AbstractKafkaSchemaSerDeConfig.RULE_EXECUTORS + ".exec.param." + TEST_CLIENT,
+    props.put(AbstractKafkaSchemaSerDeConfig.RULE_EXECUTORS + ".rule1.param." + TEST_CLIENT,
+        testClient);
+    props.put(AbstractKafkaSchemaSerDeConfig.RULE_EXECUTORS + ".rule2.class",
+        AzureFieldEncryptionExecutor.class.getName());
+    props.put(AbstractKafkaSchemaSerDeConfig.RULE_EXECUTORS + ".rule2.param." + TEST_CLIENT,
         testClient);
     return props;
   }
