44f7e4d27f11008d3ebe15f1a3161e2069f59d2d
Robert Yokota <rayokota@gmail.com>
2023-04-14 16:42:24 -0700

    MINOR add a CEL_FIELD test with a complex Avro schema

--- schema-rules/src/test/java/io/confluent/kafka/schemaregistry/rules/cel/CelExecutorTest.java
+++ schema-rules/src/test/java/io/confluent/kafka/schemaregistry/rules/cel/CelExecutorTest.java
@@ -57,6 +57,7 @@ import io.confluent.kafka.serializers.json.KafkaJsonSchemaDeserializer;
 import io.confluent.kafka.serializers.json.KafkaJsonSchemaSerializer;
 import io.confluent.kafka.serializers.protobuf.KafkaProtobufDeserializer;
 import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializer;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -67,6 +68,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericFixed;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.IndexedRecord;
 import org.apache.avro.reflect.ReflectData;
@@ -149,6 +151,7 @@ public class CelExecutorTest {
     Schema schema = parser.parse(enumSchema);
     return schema;
   }
+
   private Schema createUserSchema() {
     String userSchema = "{\"namespace\": \"example.avro\", \"type\": \"record\", "
         + "\"name\": \"User\","
@@ -214,6 +217,82 @@ public class CelExecutorTest {
     return schema;
   }
 
+  private Schema createFixedSchema() {
+    String fixedSchema = "{\"name\": \"Fixed\",\n"
+        + "   \"type\": \"fixed\",\n"
+        + "  \"size\" : 4\n"
+        + "}";
+    Schema.Parser parser = new Schema.Parser();
+    Schema schema = parser.parse(fixedSchema);
+    return schema;
+  }
+
+  private Schema createComplexSchema() {
+    return new Schema.Parser().parse(
+        "{\"namespace\": \"namespace\",\n"
+            + " \"type\": \"record\",\n"
+            + " \"name\": \"test\",\n"
+            + " \"fields\": [\n"
+            + "     {\"name\": \"null\", \"type\": \"null\"},\n"
+            + "     {\"name\": \"boolean\", \"type\": \"boolean\"},\n"
+            + "     {\"name\": \"int\", \"type\": \"int\"},\n"
+            + "     {\"name\": \"long\", \"type\": \"long\"},\n"
+            + "     {\"name\": \"float\", \"type\": \"float\"},\n"
+            + "     {\"name\": \"double\", \"type\": \"double\"},\n"
+            + "     {\"name\": \"bytes\", \"type\": \"bytes\"},\n"
+            + "     {\"name\": \"string\", \"type\": \"string\", \"aliases\": [\"string_alias\"]},\n"
+            + "     {\"name\": \"enum\",\n"
+            + "       \"type\": {\n"
+            + "         \"name\": \"Kind\",\n"
+            + "         \"type\": \"enum\",\n"
+            + "         \"symbols\" : [\"ONE\", \"TWO\", \"THREE\"]\n"
+            + "       }\n"
+            + "     },\n"
+            + "     {\"name\": \"array\",\n"
+            + "       \"type\": {\n"
+            + "         \"type\": \"array\",\n"
+            + "         \"items\" : \"string\"\n"
+            + "       }\n"
+            + "     },\n"
+            + "     {\"name\": \"map\",\n"
+            + "       \"type\": {\n"
+            + "         \"type\": \"map\",\n"
+            + "         \"values\" : \"string\"\n"
+            + "       }\n"
+            + "     },\n"
+            + "     {\"name\": \"union\", \"type\": [\"null\", \"string\"]},\n"
+            + "     {\"name\": \"fixed\",\n"
+            + "       \"type\": {\n"
+            + "         \"name\": \"Fixed\",\n"
+            + "         \"type\": \"fixed\",\n"
+            + "         \"size\" : 4\n"
+            + "       }\n"
+            + "     }\n"
+            + "]\n"
+            + "}");
+  }
+
+  private IndexedRecord createComplexRecord() {
+    Schema enumSchema = createEnumSchema();
+    Schema fixedSchema = createFixedSchema();
+    Schema schema = createComplexSchema();
+    GenericRecord avroRecord = new GenericData.Record(schema);
+    avroRecord.put("null", null);
+    avroRecord.put("boolean", true);
+    avroRecord.put("int", 1);
+    avroRecord.put("long", 2L);
+    avroRecord.put("float", 3.0f);
+    avroRecord.put("double", 4.0d);
+    avroRecord.put("bytes", ByteBuffer.wrap(new byte[]{0, 1, 2}));
+    avroRecord.put("string", "testUser");
+    avroRecord.put("enum", new GenericData.EnumSymbol(enumSchema, "ONE"));
+    avroRecord.put("array", ImmutableList.of("hi", "there"));
+    avroRecord.put("map", ImmutableMap.of("bye", "there"));
+    avroRecord.put("union", "zap");
+    avroRecord.put("fixed", new GenericData.Fixed(fixedSchema, new byte[]{0, 0, 0, 0}));
+    return avroRecord;
+  }
+
   @Test
   public void testKafkaAvroSerializer() throws Exception {
     IndexedRecord avroRecord = createUserRecord();
@@ -243,6 +322,22 @@ public class CelExecutorTest {
     byte[] bytes = avroSerializer.serialize(topic, avroRecord);
     GenericRecord obj = (GenericRecord) avroDeserializer.deserialize(topic, bytes);
     assertEquals("testUser-suffix", obj.get("name").toString());
+  }
+
+  @Test
+  public void testKafkaAvroSerializerFieldTransformComplex() throws Exception {
+    IndexedRecord avroRecord = createComplexRecord();
+    AvroSchema avroSchema = new AvroSchema(avroRecord.getSchema());
+    Rule rule = new Rule("myRule", null, RuleKind.TRANSFORM, RuleMode.WRITE,
+        CelFieldExecutor.TYPE, null, null, "name == \"string\" ; value + \"-suffix\"",
+        null, null, false);
+    RuleSet ruleSet = new RuleSet(Collections.emptyList(), Collections.singletonList(rule));
+    avroSchema = avroSchema.copy(null, ruleSet);
+    schemaRegistry.register(topic + "-value", avroSchema);
+
+    byte[] bytes = avroSerializer.serialize(topic, avroRecord);
+    GenericRecord obj = (GenericRecord) avroDeserializer.deserialize(topic, bytes);
+    assertEquals("testUser-suffix", obj.get("string").toString());
   }
 
   @Test(expected = SerializationException.class)
