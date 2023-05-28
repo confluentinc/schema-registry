6835f2467a56896a80843d050eb0eadb38a1e281
Robert Yokota <rayokota@gmail.com>
2023-05-28 16:56:57 -0700

    Fix checkstyle

--- client-encryption/src/main/java/io/confluent/kafka/schemaregistry/encryption/local/LocalFieldEncryptionExecutor.java
+++ client-encryption/src/main/java/io/confluent/kafka/schemaregistry/encryption/local/LocalFieldEncryptionExecutor.java
@@ -24,7 +24,6 @@ import java.security.GeneralSecurityException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 
