96c06d4192e65187092eff9882a184c0aef7a63b
Robert Yokota <rayokota@gmail.com>
2023-10-16 17:18:39 -0700

    Add good dek gen test

--- client-encryption/src/test/java/io/confluent/kafka/schemaregistry/encryption/FieldEncryptionExecutorTest.java
+++ client-encryption/src/test/java/io/confluent/kafka/schemaregistry/encryption/FieldEncryptionExecutorTest.java
@@ -119,8 +119,8 @@ public abstract class FieldEncryptionExecutorTest {
   private final KafkaProtobufDeserializer<DynamicMessage> protobufDeserializer;
   private final KafkaAvroSerializer badSerializer;
   private final KafkaAvroDeserializer badDeserializer;
+  private final KafkaAvroSerializer goodDekSerializer;
   private final KafkaAvroSerializer badDekSerializer;
-  private final KafkaAvroDeserializer badDekDeserializer;
   private final String topic;
 
   public FieldEncryptionExecutorTest() throws Exception {
@@ -183,10 +183,13 @@ public abstract class FieldEncryptionExecutorTest {
     badSerializer = new KafkaAvroSerializer(schemaRegistry, badClientProps);
     badDeserializer = new KafkaAvroDeserializer(schemaRegistry, badClientProps);
 
+    FieldEncryptionProperties goodDekProps = getFieldEncryptionProperties(ruleNames, GoodDekGenerator.class);
+    Map<String, Object> goodDekClientProps = goodDekProps.getClientProperties("mock://");
+    goodDekSerializer = new KafkaAvroSerializer(schemaRegistry, goodDekClientProps);
+
     FieldEncryptionProperties badDekProps = getFieldEncryptionProperties(ruleNames, BadDekGenerator.class);
     Map<String, Object> badDekClientProps = badDekProps.getClientProperties("mock://");
     badDekSerializer = new KafkaAvroSerializer(schemaRegistry, badDekClientProps);
-    badDekDeserializer = new KafkaAvroDeserializer(schemaRegistry, badDekClientProps);
   }
 
   protected abstract FieldEncryptionProperties getFieldEncryptionProperties(
@@ -1272,6 +1275,29 @@ public abstract class FieldEncryptionExecutorTest {
   }
 
   @Test
+  public void testGoodDekGenerator() throws Exception {
+    IndexedRecord avroRecord = createUserRecord();
+    AvroSchema avroSchema = new AvroSchema(createUserSchema());
+    Rule rule = new Rule("rule1", null, null, null,
+        FieldEncryptionExecutor.TYPE, ImmutableSortedSet.of("PII"), null, null, null, null, false);
+    RuleSet ruleSet = new RuleSet(Collections.emptyList(), ImmutableList.of(rule));
+    Metadata metadata = getMetadata("kek1");
+    avroSchema = avroSchema.copy(metadata, ruleSet);
+    schemaRegistry.register(topic + "-value", avroSchema);
+
+
+    int expectedEncryptions = 1;
+    RecordHeaders headers = new RecordHeaders();
+    Cryptor cryptor = addSpyToCryptor(goodDekSerializer);
+    byte[] bytes = goodDekSerializer.serialize(topic, headers, avroRecord);
+    verify(cryptor, times(expectedEncryptions)).encrypt(any(), any(), any());
+    cryptor = addSpyToCryptor(avroDeserializer);
+    GenericRecord record = (GenericRecord) avroDeserializer.deserialize(topic, headers, bytes);
+    verify(cryptor, times(expectedEncryptions)).decrypt(any(), any(), any());
+    assertEquals("testUser", record.get("name"));
+  }
+
+  @Test
   public void testBadDekGenerator() throws Exception {
     IndexedRecord avroRecord = createUserRecord();
     AvroSchema avroSchema = new AvroSchema(createUserSchema());
@@ -1526,6 +1552,15 @@ public abstract class FieldEncryptionExecutorTest {
     @Override
     public int hashCode() {
       return Objects.hash(annotatedPii);
+    }
+  }
+
+  public static class GoodDekGenerator extends FieldEncryptionExecutor {
+
+    @Override
+    protected byte[] generateDek(DekFormat dekFormat) throws GeneralSecurityException {
+      // generate a valid dek
+      return new byte[32];
     }
   }
 
