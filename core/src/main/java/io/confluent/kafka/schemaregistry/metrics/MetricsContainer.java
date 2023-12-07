ee0a6901c5d3f7aa974f8d75602400f27a0db60f
andrewxding <andrewxding@yahoo.com>
2023-12-07 15:33:37 -0800

    Fix jmx prefix default

--- core/src/main/java/io/confluent/kafka/schemaregistry/metrics/MetricsContainer.java
+++ core/src/main/java/io/confluent/kafka/schemaregistry/metrics/MetricsContainer.java
@@ -19,8 +19,10 @@ import io.confluent.kafka.schemaregistry.avro.AvroSchema;
 import io.confluent.kafka.schemaregistry.json.JsonSchema;
 import io.confluent.kafka.schemaregistry.protobuf.ProtobufSchema;
 import io.confluent.kafka.schemaregistry.rest.SchemaRegistryConfig;
+import io.confluent.kafka.schemaregistry.utils.AppInfoParser;
 import io.confluent.rest.Application;
 import io.confluent.rest.RestConfig;
+import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.MetricName;
 import org.apache.kafka.common.metrics.JmxReporter;
@@ -234,8 +236,17 @@ public class MetricsContainer {
   private static MetricsContext buildMetricsContext(
           SchemaRegistryConfig config, String kafkaClusterId) {
 
-    Map<String, String> metadata = config.getMetricsContext().contextLabels();
+    String srGroupId = config.getString(SchemaRegistryConfig.SCHEMAREGISTRY_GROUP_ID_CONFIG);
+
+    Map<String, Object> metadata =
+            config.originalsWithPrefix(CommonClientConfigs.METRICS_CONTEXT_PREFIX);
+
     metadata.put(RESOURCE_LABEL_KAFKA_CLUSTER_ID, kafkaClusterId);
+    metadata.put(config.RESOURCE_LABEL_CLUSTER_ID, srGroupId);
+    metadata.put(config.RESOURCE_LABEL_GROUP_ID, srGroupId);
+    metadata.put(config.RESOURCE_LABEL_TYPE,  "schema_registry");
+    metadata.put(config.RESOURCE_LABEL_VERSION, AppInfoParser.getVersion());
+    metadata.put(config.RESOURCE_LABEL_COMMIT_ID, AppInfoParser.getCommitId());
 
     return new KafkaMetricsContext(JMX_PREFIX, metadata);
   }
