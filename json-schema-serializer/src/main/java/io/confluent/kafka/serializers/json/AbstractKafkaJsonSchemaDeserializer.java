01d8b72bc8eeb6994661bca46d1a87809b160166
Robert Yokota <rayokota@gmail.com>
2024-04-04 12:42:07 -0700

    DGS-10533 Fix NPE in deser when use.latest=true and RecordNameStrategy

--- json-schema-serializer/src/main/java/io/confluent/kafka/serializers/json/AbstractKafkaJsonSchemaDeserializer.java
+++ json-schema-serializer/src/main/java/io/confluent/kafka/serializers/json/AbstractKafkaJsonSchemaDeserializer.java
@@ -131,6 +131,9 @@ public abstract class AbstractKafkaJsonSchemaDeserializer<T> extends AbstractKaf
       String subject = isKey == null || strategyUsesSchema(isKey)
           ? getContextName(topic) : subjectName(topic, isKey, null);
       JsonSchema schema = ((JsonSchema) schemaRegistry.getSchemaBySubjectAndId(subject, id));
+      if (isKey == null || strategyUsesSchema(isKey)) {
+        subject = subjectName(topic, isKey, schema);
+      }
 
       ParsedSchema readerSchema = null;
       if (metadata != null) {
