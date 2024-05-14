f950e9f5332257ff1293370585d4a739a719b95c
Robert Yokota <rayokota@gmail.com>
2024-05-14 16:00:15 -0700

    Minor cleanup

--- dek-registry/src/main/java/io/confluent/dekregistry/web/rest/resources/DekRegistryResource.java
+++ dek-registry/src/main/java/io/confluent/dekregistry/web/rest/resources/DekRegistryResource.java
@@ -50,7 +50,6 @@ import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.SortedMap;
