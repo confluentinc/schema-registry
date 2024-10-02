036d99528fc538b10b844ceaae873cde4361b397
Kipp Corman <kcorman@confluent.io>
2024-10-02 14:13:03 -0500

    DGS-15999 Add performanceMetric to list schema refs API (#3264)

--- core/src/main/java/io/confluent/kafka/schemaregistry/rest/resources/SubjectVersionsResource.java
+++ core/src/main/java/io/confluent/kafka/schemaregistry/rest/resources/SubjectVersionsResource.java
@@ -181,6 +181,7 @@ public class SubjectVersionsResource {
 
   @GET
   @Path("/{version}/referencedby")
+  @PerformanceMetric("subjects.versions.list-schema-refs")
   @Operation(summary = "List schemas referencing a schema",
       description = "Retrieves the IDs of schemas that reference the specified schema.",
       responses = {
