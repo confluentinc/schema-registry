3ea62dc592b877128698abb5f68710efb8e918bd
Manikumar Reddy <manikumar.reddy@gmail.com>
2024-10-19 20:54:51 +0530

    remove ununsed imports

--- core/src/test/java/io/confluent/kafka/schemaregistry/SASLClusterTestHarness.java
+++ core/src/test/java/io/confluent/kafka/schemaregistry/SASLClusterTestHarness.java
@@ -27,8 +27,6 @@ import org.junit.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import scala.Option;
-import scala.collection.JavaConverters;
-import scala.collection.immutable.Seq;
 
 import javax.security.auth.login.Configuration;
 import java.io.File;
