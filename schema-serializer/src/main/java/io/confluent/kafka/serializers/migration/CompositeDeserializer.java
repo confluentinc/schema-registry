66c8e144ce267b6aa37c5c9a4436d9566e54249d
Robert Yokota <rayokota@gmail.com>
2024-11-28 08:53:46 -0800

    Add composite deserializer for migration

---
+++ schema-serializer/src/main/java/io/confluent/kafka/serializers/migration/CompositeDeserializer.java
@@ -0,0 +1,138 @@
+/*
+ * Copyright 2024 Confluent Inc.
+ *
+ * Licensed under the Confluent Community License (the "License"); you may not use
+ * this file except in compliance with the License.  You may obtain a copy of the
+ * License at
+ *
+ * http://www.confluent.io/confluent-community-license
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OF ANY KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ *
+ */
+
+package io.confluent.kafka.serializers.migration;
+
+import io.confluent.kafka.schemaregistry.ParsedSchema;
+import io.confluent.kafka.schemaregistry.client.SchemaRegistryClient;
+import io.confluent.kafka.schemaregistry.client.rest.exceptions.RestClientException;
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDe;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.nio.ByteBuffer;
+import java.util.Map;
+import org.apache.commons.lang3.SerializationException;
+import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CompositeDeserializer implements Deserializer<Object> {
+
+  protected Logger log = LoggerFactory.getLogger(CompositeDeserializer.class);
+
+  protected static final byte MAGIC_BYTE = 0x0;
+
+  private Deserializer oldDeserializer;
+  private Deserializer confluentDeserializer;
+  private SchemaRegistryClient schemaRegistryClient;
+
+  /**
+   * Constructor used by Kafka consumer.
+   */
+  public CompositeDeserializer() {
+  }
+
+  @Override
+  public void configure(Map<String, ?> configs, boolean isKey) {
+    configure(new CompositeDeserializerConfig(configs), isKey);
+  }
+
+  @SuppressWarnings("unchecked")
+  protected void configure(CompositeDeserializerConfig config, boolean isKey) {
+    this.oldDeserializer = config.getConfiguredInstance(
+        CompositeDeserializerConfig.OLD_DESERIALIZER, Deserializer.class);
+    this.confluentDeserializer = config.getConfiguredInstance(
+        CompositeDeserializerConfig.CONFLUENT_DESERIALIZER, Deserializer.class);
+    this.schemaRegistryClient = getSchemaRegistryClient();
+  }
+
+  @Override
+  public Object deserialize(String topic, byte[] bytes) {
+    return deserialize(topic, null, bytes);
+  }
+
+  @Override
+  public Object deserialize(String topic, Headers headers, byte[] bytes) {
+    try {
+      if (bytes == null) {
+        return null;
+      }
+
+      // We assume TopicNameStrategy
+      String subject = topic + "-value";
+
+      int schemaId = getSchemaId(ByteBuffer.wrap(bytes));
+
+      if (isValidSchemaId(subject, schemaId)) {
+        return confluentDeserializer.deserialize(topic, headers, bytes);
+      } else {
+        return oldDeserializer.deserialize(topic, bytes);
+      }
+    } catch (Exception e) {
+      throw new SerializationException(e);
+    }
+  }
+
+  private int getSchemaId(ByteBuffer payload) {
+    if (payload == null) {
+      // Skip validation if no strategy is specified or if no data is present.
+      return 0;
+    }
+    if (payload.get() != MAGIC_BYTE) {
+      return 0;
+    }
+    return payload.getInt();
+  }
+
+  protected boolean isValidSchemaId(String subject, int id)
+      throws IOException, RestClientException {
+    if (id == 0) {
+      return false;
+    }
+
+    // Obtain the schema.
+    ParsedSchema schema = schemaRegistryClient.getSchemaBySubjectAndId(subject, id);
+
+    // Get the id of the schema that was saved in the registry.
+    int savedId = schemaRegistryClient.getId(subject, schema);
+
+    return id == savedId;
+  }
+
+  @SuppressWarnings("unchecked")
+  private SchemaRegistryClient getSchemaRegistryClient() {
+    // We use reflection to access the SR client as it is a protected member right now
+    // We should consider making it public in the future
+    try {
+      Field f = AbstractKafkaSchemaSerDe.class.getDeclaredField("schemaRegistry");
+      f.setAccessible(true);
+      return (SchemaRegistryClient) f.get(confluentDeserializer);
+    } catch (NoSuchFieldException | IllegalAccessException e) {
+      throw new RuntimeException(e);
+    }
+  }
+  @Override
+  public void close() {
+    try {
+      oldDeserializer.close();
+      confluentDeserializer.close();
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
