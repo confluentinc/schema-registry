be2849f0938e99710df1fc3903b6a01dfc915c4f
Kunal Gupta <kugupta@confluent.io>
2024-12-13 16:12:24 -0600

    add logs

--- .semaphore/sonarqube-wrapper.sh
+++ .semaphore/sonarqube-wrapper.sh
@@ -1,5 +1,7 @@
 set -e
 
+echo "Starting SonarQube scanning workflow"
+
 modules=("avro-converter" "avro-data" "avro-serde" "avro-serializer" "benchmark" "client" "client-console-scripts" "core" "json-schema-converter" "json-schema-provider" "json-schema-serde" "json-schema-serializer" "json-serializer" "maven-plugin" "protobuf-converter" "protobuf-provider" "protobuf-serde" "protobuf-serializer" "protobuf-types" "schema-serializer")
 
 checkout
@@ -9,3 +11,5 @@ for dir in ${modules[@]}; do
   echo "Scanning $dir"
   emit-sonarqube-data --run_only_sonar_scan
 done
+
+echo "SonarQube scanning workflow completed"
