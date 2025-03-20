f84870b3e8d76ed14f7ea1cbddbb3676de70e3f0
Robert Yokota <rayokota@gmail.com>
2025-03-19 17:49:26 -0700

    Upgrade to Jetty 12, use jakarta APIs

--- core/src/main/java/io/confluent/kafka/schemaregistry/rest/resources/ContextsResource.java
+++ core/src/main/java/io/confluent/kafka/schemaregistry/rest/resources/ContextsResource.java
@@ -31,12 +31,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import io.swagger.v3.oas.annotations.tags.Tags;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DefaultValue;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DefaultValue;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import java.util.List;
 import java.util.stream.Collectors;
 
