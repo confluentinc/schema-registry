056f131448d57d4f3b56f974797b766ab58dd2da
Akhilesh Manjunath <amanjunath@confluent.io>
2025-07-02 19:18:09 -0700

    DGS-21256: fix license headers

--- core/src/test/java/io/confluent/kafka/schemaregistry/rest/SchemaRegistryExtensionTest.java
+++ core/src/test/java/io/confluent/kafka/schemaregistry/rest/SchemaRegistryExtensionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Confluent Inc.
+ * Copyright 2017-2020 Confluent Inc.
  *
  * Licensed under the Confluent Community License (the "License"); you may not use
  * this file except in compliance with the License.  You may obtain a copy of the
