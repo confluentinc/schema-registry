d4e673a67ada6ab1d939a67fb92e6893ebd4edb7
Akhilesh Manjunath <amanjunath@confluent.io>
2025-07-02 22:28:44 -0700

    DGS-21256: fix license headers

--- client/src/test/java/io/confluent/kafka/schemaregistry/client/CachedSchemaRegistryClientTest.java
+++ client/src/test/java/io/confluent/kafka/schemaregistry/client/CachedSchemaRegistryClientTest.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.confluent.kafka.schemaregistry.client;
 
 import com.google.common.testing.FakeTicker;
