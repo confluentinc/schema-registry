49586b55d380d4a61ac81cc125681dcc5d885594
Akhilesh Manjunath <amanjunath@confluent.io>
2025-07-08 13:08:16 -0700

    DGS-21256: support for license headers (#3783)

--- json-schema-provider/src/main/java/io/confluent/kafka/schemaregistry/json/JsonSchema.java
+++ json-schema-provider/src/main/java/io/confluent/kafka/schemaregistry/json/JsonSchema.java
@@ -13,7 +13,6 @@
  * specific language governing permissions and limitations under the License.
  */
 
-
 package io.confluent.kafka.schemaregistry.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
