523ec6a38187bcfc9a5cec764ef6d6f184a729b4
Akhilesh Manjunath <amanjunath@confluent.io>
2025-07-09 17:57:33 -0700

    DGS-21256: fix license headers

--- client/src/test/java/io/confluent/kafka/schemaregistry/client/rest/ApacheClientRestServiceTest.java
+++ client/src/test/java/io/confluent/kafka/schemaregistry/client/rest/ApacheClientRestServiceTest.java
@@ -1,4 +1,3 @@
-
 /*
  * Copyright 2025 Confluent Inc.
  *
@@ -14,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.confluent.kafka.schemaregistry.client.rest;
 
 import static junit.framework.TestCase.assertEquals;
