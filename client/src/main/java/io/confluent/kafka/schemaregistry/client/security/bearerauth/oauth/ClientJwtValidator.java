7256aa3d8581039c05e45554e3b26e64832c67e8
Akhilesh Manjunath <amanjunath@confluent.io>
2025-07-09 18:17:09 -0700

    DGS-21256: fix license headers (#3800)

--- client/src/main/java/io/confluent/kafka/schemaregistry/client/security/bearerauth/oauth/ClientJwtValidator.java
+++ client/src/main/java/io/confluent/kafka/schemaregistry/client/security/bearerauth/oauth/ClientJwtValidator.java
@@ -12,8 +12,6 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
- * This is copy of the class from the Apache Kafka project.
  */
 
 package io.confluent.kafka.schemaregistry.client.security.bearerauth.oauth;
