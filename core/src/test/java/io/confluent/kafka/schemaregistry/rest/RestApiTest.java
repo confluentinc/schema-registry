c9218f66f8f4c5d58be78b1f1048fb922aff08c4
gzimmers <gzimmers@confluentinc.com>
2025-08-12 10:38:21 -0600

    patch: fix referential integrity bug with schema revival

--- core/src/test/java/io/confluent/kafka/schemaregistry/rest/RestApiTest.java
+++ core/src/test/java/io/confluent/kafka/schemaregistry/rest/RestApiTest.java
@@ -1978,5 +1978,64 @@ public class RestApiTest extends ClusterTestHarness {
     // Join base URL and path, collapsing any duplicate forward slash delimiters
     return baseUrl.replaceFirst("/$", "") + "/" + path.replaceFirst("^/", "");
   }
-}
 
+  @Test
+  public void testReviveSchemaWithDanglingReference() throws Exception {
+    // Step 1: Register a schema ref
+    String refSchema = "{\"type\":\"record\",\"name\":\"Ref\",\"namespace\":\"com.example\",\"fields\":[{\"name\":\"id\",\"type\":\"string\"}]}";
+    int refId = restApp.restClient.registerSchema(refSchema, "ref");
+    assertEquals("Ref schema should be registered with id 1", 1, refId);
+
+    // Step 2: Register a schema base with reference to ref
+    String baseSchema = "{\"type\":\"record\",\"name\":\"Base\",\"namespace\":\"com.example\",\"fields\":[{\"name\":\"refField\",\"type\":\"com.example.Ref\"}]}";
+    RegisterSchemaRequest baseRequest = new RegisterSchemaRequest();
+    baseRequest.setSchema(baseSchema);
+    SchemaReference ref = new SchemaReference("com.example.Ref", "ref", 1);
+    baseRequest.setReferences(Collections.singletonList(ref));
+    
+    int baseId = restApp.restClient.registerSchema(baseRequest, "base", false);
+    assertEquals("Base schema should be registered with id 2", 2, baseId);
+
+    // Step 3: Soft delete base
+    assertEquals("Soft deleting base schema should succeed", 
+        (Integer) 1, 
+        restApp.restClient.deleteSchemaVersion(RestService.DEFAULT_REQUEST_PROPERTIES, "base", "1"));
+
+    // Step 4: Soft delete ref
+    assertEquals("Soft deleting ref schema should succeed", 
+        (Integer) 1, 
+        restApp.restClient.deleteSchemaVersion(RestService.DEFAULT_REQUEST_PROPERTIES, "ref", "1"));
+
+    // Step 5: Hard delete ref
+    assertEquals("Hard deleting ref schema should succeed", 
+        (Integer) 1, 
+        restApp.restClient.deleteSchemaVersion(RestService.DEFAULT_REQUEST_PROPERTIES, "ref", "1", true));
+
+    // Step 6: Re-register base to "revive" it 
+    // BUG FIXED - so now fail validation
+    try {
+      restApp.restClient.registerSchema(baseRequest, "base", false);
+      fail("Re-registering base schema with dangling reference should fail");
+    } catch (RestClientException rce) {
+      assertEquals("Should fail with invalid schema error", 
+          Errors.INVALID_SCHEMA_ERROR_CODE, 
+          rce.getErrorCode());
+    }
+    // Step 7: Try to register a modified version of base - this should fail
+    String modifiedBaseSchema = "{\"type\":\"record\",\"name\":\"Base\",\"namespace\":\"com.example\",\"fields\":[{\"name\":\"refField\",\"type\":\"com.example.Ref\"},{\"name\":\"newField\",\"type\":\"string\"}]}";
+    RegisterSchemaRequest modifiedRequest = new RegisterSchemaRequest();
+    modifiedRequest.setSchema(modifiedBaseSchema);
+    modifiedRequest.setReferences(Collections.singletonList(ref));
+
+    try {
+      restApp.restClient.registerSchema(modifiedRequest, "base", false);
+      fail("Registering modified schema with dangling reference should fail");
+    } catch (RestClientException rce) {
+      assertEquals("Should fail with invalid schema error", 
+          Errors.INVALID_SCHEMA_ERROR_CODE, 
+          rce.getErrorCode());
+      assertTrue("Error message should mention missing reference", 
+          rce.getMessage().contains("Failed to validate reference"));
+    }
+  }
+}
