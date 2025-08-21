47981a3f7e3f5b7f1217a7014629da10db6cf650
Greg Zimmers <gzimmers@confluent.io>
2025-08-21 17:34:43 -0600

    add get sr server version to sr client API (#3870)
    Co-authored-by: gzimmers <gzimmers@confluentinc.com>

--- core/src/test/java/io/confluent/kafka/schemaregistry/rest/client/LocalSchemaRegistryClientTest.java
+++ core/src/test/java/io/confluent/kafka/schemaregistry/rest/client/LocalSchemaRegistryClientTest.java
@@ -214,4 +214,73 @@ public class LocalSchemaRegistryClientTest extends ClusterTestHarness {
         Schema s2 = client.getByVersion(SUBJECT2, 1, false);
         assertEquals(id2, s2.getId().intValue());
     }
+=======
+import static io.confluent.kafka.schemaregistry.storage.SchemaRegistry.DEFAULT_TENANT;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import io.confluent.kafka.schemaregistry.SchemaProvider;
+import io.confluent.kafka.schemaregistry.avro.AvroSchemaProvider;
+import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaRegistryDeployment;
+import io.confluent.kafka.schemaregistry.client.rest.entities.SchemaRegistryServerVersion;
+import io.confluent.kafka.schemaregistry.storage.KafkaSchemaRegistry;
+import io.confluent.kafka.schemaregistry.utils.Props;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LocalSchemaRegistryClientTest {
+
+  @Mock
+  private KafkaSchemaRegistry mockSchemaRegistry;
+
+  @Mock
+  private SchemaProvider mockSchemaProvider;
+
+  @Mock
+  private AvroSchemaProvider mockAvroProvider;
+
+  private LocalSchemaRegistryClient client;
+  private String testSchemaString;
+
+  @Before
+  public void setUp() {
+    when(mockSchemaRegistry.tenant()).thenReturn(DEFAULT_TENANT);
+    client = new LocalSchemaRegistryClient(mockSchemaRegistry);
+  }
+
+  @Test
+  public void testGetSchemaRegistryDeployment() throws Exception {
+    Map<String, Object> props = new HashMap<>();
+    List<String> deploymentAttributes = new ArrayList<String>(Collections.singleton("deploymentScope:opensource"));
+    props.put(Props.PROPERTY_SCHEMA_REGISTRY_DEPLOYMENT_ATTRIBUTES, deploymentAttributes);
+    when(mockSchemaRegistry.properties()).thenReturn(props);
+
+    SchemaRegistryDeployment deployment = client.getSchemaRegistryDeployment();
+
+    assertNotNull(deployment);
+    assertEquals(deployment.getAttributes(),
+        new ArrayList<String>(Collections.singleton("deploymentScope:opensource"))
+    );
+  }
+
+  @Test
+  public void testGetSchemaRegistryServerVersion() throws Exception {
+    SchemaRegistryServerVersion version = client.getSchemaRegistryServerVersion();
+
+    assertNotNull(version);
+    assertNotNull(version.getVersion());
+    assertNotNull(version.getCommitId());
+  }
+>>>>>>> d9bfbb7af61 (add get sr server version to sr client API (#3870))
 }
