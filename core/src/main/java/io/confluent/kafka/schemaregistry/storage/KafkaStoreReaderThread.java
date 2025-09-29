40c8761d431ccd68591241ff847b67768c006db6
Kunal Gupta <kugupta@confluent.io>
2025-09-29 01:22:49 -0500

    don't need checkpoints

--- core/src/main/java/io/confluent/kafka/schemaregistry/storage/KafkaStoreReaderThread.java
+++ core/src/main/java/io/confluent/kafka/schemaregistry/storage/KafkaStoreReaderThread.java
@@ -119,8 +119,6 @@ public class KafkaStoreReaderThread<K, V> extends ShutdownableThread {
         throw new IllegalStateException("Failed to read checkpoints", e);
       }
       log.info("Successfully read checkpoints");
-
-      this.storeUpdateHandler.preBootstrap(checkpointFileCache);
     }
 
     KafkaStore.addSchemaRegistryConfigsToClientProperties(config, consumerProps);
