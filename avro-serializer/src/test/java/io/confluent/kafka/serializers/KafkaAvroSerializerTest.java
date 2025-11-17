c4fff5caffdc9cdc5b5f645a416037454893b5ee
Robert Yokota <rayokota@gmail.com>
2025-11-17 13:43:26 -0800

    Revert "Upgrade to Avro 1.12.1 (#3953)"
    This reverts commit 4ce8891a5525ac82bf37a4989089a87d3c3e6c3a.

--- avro-serializer/src/test/java/io/confluent/kafka/serializers/KafkaAvroSerializerTest.java
+++ avro-serializer/src/test/java/io/confluent/kafka/serializers/KafkaAvroSerializerTest.java
@@ -282,7 +282,7 @@ public class KafkaAvroSerializerTest {
           + " \"name\": \"test\",\n"
           + " \"items\": {\n"
           + "\"type\": \"record\",\n"
-          + "\"namespace\": \"io.confluent.kafka.example\",\n"
+          + "\"namespace\": \"example.avro\",\n"
           + "\"name\": \"User\",\n"
           + "\"fields\": [{\"name\": \"name\", \"type\": \"string\"}]}}");
 
@@ -292,7 +292,7 @@ public class KafkaAvroSerializerTest {
           + " \"name\": \"test\",\n"
           + " \"values\": {\n"
           + "\"type\": \"record\",\n"
-          + "\"namespace\": \"io.confluent.kafka.example\",\n"
+          + "\"namespace\": \"example.avro\",\n"
           + "\"name\": \"User\",\n"
           + "\"fields\": [{\"name\": \"name\", \"type\": \"string\"}]}}");
 
@@ -930,7 +930,6 @@ public class KafkaAvroSerializerTest {
             new SchemaReference("io.confluent.kafka.example.User", "user", -1)
         )));
   }
-
   @Test
   public void testKafkaAvroSerializerWithArraySpecific() throws IOException, RestClientException {
     Map serializerConfigs = ImmutableMap.of(
@@ -977,7 +976,7 @@ public class KafkaAvroSerializerTest {
         true
     );
     Map<Utf8, IndexedRecord> data = new HashMap<>();
-    data.put(new Utf8("one"), createSpecificAvroRecord());
+    data.put(new Utf8("one"), createUserRecordUtf8());
     schemaRegistry.register(topic + "-value", new AvroSchema(mapSchema));
     avroSerializer.configure(serializerConfigs, false);
     avroDeserializer.configure(deserializerConfigs, false);
