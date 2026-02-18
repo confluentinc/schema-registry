38706cc03ca506663b9b2f7e8737ebe66f30197d
Gremi64 <gremi64@users.noreply.github.com>
2026-02-18 17:19:47 +0000

    defaultToGlobal available in SRClient

--- client/src/main/java/io/confluent/kafka/schemaregistry/client/SchemaRegistryClient.java
+++ client/src/main/java/io/confluent/kafka/schemaregistry/client/SchemaRegistryClient.java
@@ -263,6 +263,10 @@ public interface SchemaRegistryClient extends Closeable, SchemaVersionFetcher {
     return getConfig(subject).getCompatibilityLevel();
   }
 
+  default String getCompatibility(String subject, boolean defaultToGlobal) throws IOException, RestClientException {
+    return getConfig(subject, defaultToGlobal).getCompatibilityLevel();
+  }
+
   default void deleteCompatibility(String subject) throws IOException, RestClientException {
     deleteConfig(subject);
   }
@@ -273,6 +277,10 @@ public interface SchemaRegistryClient extends Closeable, SchemaVersionFetcher {
   }
 
   default Config getConfig(String subject) throws IOException, RestClientException {
+    throw new UnsupportedOperationException();
+  }
+
+  default Config getConfig(String subject, boolean defaultToGlobal) throws IOException, RestClientException {
     throw new UnsupportedOperationException();
   }
 
