16536bc94a76bf04a219b64315b21c286c563366
Robert Yokota <rayokota@gmail.com>
2026-02-18 17:01:03 -0800

    DGS-22427 Add additional association request validation

--- client/src/main/java/io/confluent/kafka/schemaregistry/client/rest/entities/requests/AssociationCreateOrUpdateInfo.java
+++ client/src/main/java/io/confluent/kafka/schemaregistry/client/rest/entities/requests/AssociationCreateOrUpdateInfo.java
@@ -163,7 +163,9 @@ public class AssociationCreateOrUpdateInfo {
     } else {
       setAssociationType(VALUE_ASSOCIATION_TYPE);
     }
-    if (getLifecycle() == LifecyclePolicy.WEAK) {
+    if (getLifecycle() == null) {
+      throw new IllegalPropertyException("lifecycle", "cannot be null");
+    } else if (getLifecycle() == LifecyclePolicy.WEAK) {
       if (Boolean.TRUE.equals(getFrozen())) {
         throw new IllegalPropertyException(
             "frozen", "association with lifecycle of WEAK cannot be frozen");
