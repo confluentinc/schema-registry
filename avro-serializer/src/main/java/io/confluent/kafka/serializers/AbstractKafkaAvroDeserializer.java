8d4aeaf064f2900f0f739455e92768cb470c0fa6
Robert Yokota <rayokota@gmail.com>
2026-02-25 10:24:34 -0800

    Minor cleanup

--- avro-serializer/src/main/java/io/confluent/kafka/serializers/AbstractKafkaAvroDeserializer.java
+++ avro-serializer/src/main/java/io/confluent/kafka/serializers/AbstractKafkaAvroDeserializer.java
@@ -575,8 +575,6 @@ public abstract class AbstractKafkaAvroDeserializer extends AbstractKafkaSchemaS
           readerAvroSchema = writerAvroSchema;
         }
 
-        String s = getSubject();
-
         if (!migrations.isEmpty() || (readerAvroSchema.ruleSet() != null
             && !readerAvroSchema.ruleSet().getDomainRules().isEmpty())) {
           AvroSchemaUtils.setThreadLocalData(
