9c9f346d652a5eacd3a6d2c5704a3943c5dbdc0a
Robert Yokota <ryokota@confluent.io>
2026-05-12 11:30:22 -0700

    Revert "Merge pull request #4134 from confluentinc/rename-enable" (#4300)

--- schema-serializer/src/main/java/io/confluent/kafka/serializers/AbstractKafkaSchemaSerDe.java
+++ schema-serializer/src/main/java/io/confluent/kafka/serializers/AbstractKafkaSchemaSerDe.java
@@ -688,19 +688,19 @@ public abstract class AbstractKafkaSchemaSerDe implements Closeable {
     List<Rule> rules = Collections.emptyList();
     if (ruleMode == RuleMode.UPGRADE) {
       if (target.ruleSet() != null) {
-        enabledEnv = target.ruleSet().getEnableAt();
+        enabledEnv = target.ruleSet().getEnableOnlyAt();
         rules = target.ruleSet().getMigrationRules();
       }
     } else if (ruleMode == RuleMode.DOWNGRADE) {
       if (source.ruleSet() != null) {
-        enabledEnv = source.ruleSet().getEnableAt();
+        enabledEnv = source.ruleSet().getEnableOnlyAt();
         rules = new ArrayList<>(source.ruleSet().getMigrationRules());
         // Execute downgrade rules in reverse order for symmetry
         Collections.reverse(rules);
       }
     } else {
       if (target.ruleSet() != null) {
-        enabledEnv = target.ruleSet().getEnableAt();
+        enabledEnv = target.ruleSet().getEnableOnlyAt();
         rules = rulePhase == RulePhase.ENCODING
             ? target.ruleSet().getEncodingRules()
             : target.ruleSet().getDomainRules();
