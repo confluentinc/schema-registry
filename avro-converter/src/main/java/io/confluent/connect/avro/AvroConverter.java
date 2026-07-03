58fdd6b116173cba195023422b1cbf64a194880a
Sri Baabu <sbaabu@confluent.io>
2026-07-03 12:49:26 +0530

    fix: remove unused List imports from converters

--- avro-converter/src/main/java/io/confluent/connect/avro/AvroConverter.java
+++ avro-converter/src/main/java/io/confluent/connect/avro/AvroConverter.java
@@ -56,7 +56,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 /**
--- json-schema-converter/src/main/java/io/confluent/connect/json/JsonSchemaConverter.java
+++ json-schema-converter/src/main/java/io/confluent/connect/json/JsonSchemaConverter.java
@@ -51,7 +51,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 /**
