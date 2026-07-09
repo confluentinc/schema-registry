82174f3dc10cdcab8e36b04b431cb516844dde07
Robert Yokota <rayokota@gmail.com>
2026-07-08 23:41:09 -0700

    Minor fix

--- client-encryption-azure/src/main/java/io/confluent/kafka/schemaregistry/encryption/azure/AzureKmsAead.java
+++ client-encryption-azure/src/main/java/io/confluent/kafka/schemaregistry/encryption/azure/AzureKmsAead.java
@@ -183,9 +183,7 @@ public final class AzureKmsAead implements Aead {
       return false;
     }
     for (int i = 0; i < value.length(); i++) {
-      char c = value.charAt(i);
-      boolean isHexDigit = (c >= '0' && c <= '9') || (c >= 'a' && c <= 'f') || (c >= 'A' && c <= 'F');
-      if (!isHexDigit) {
+      if (Character.digit(value.charAt(i), 16) == -1) {
         return false;
       }
     }
